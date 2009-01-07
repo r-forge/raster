64fc14438723b8c311a9eaef70e825c6c947a4bb
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-01-07 03:43:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/Overlay.R
+++ pkg/raster/R/Overlay.R
@@ -5,7 +5,7 @@
 # Licence GPL v3
 
 
-Overlay <- function(raster1, raster2, fun=function(x,y){return(x+y)}, filename="", overwrite=TRUE) {
+Overlay <- function(raster1, raster2, fun=function(x,y){return(x+y)}, filename="", overwrite=FALSE) {
 	if (class(raster1) != 'RasterLayer' | class(raster2) != 'RasterLayer') {
 		stop('first two arguments should be objects of class "RasterLayer"')
 	}
