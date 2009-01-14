8925dacd62302ea2b0013dd860aa6aaeb28e69f9
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-01-14 23:54:19 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@136 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/replacement.R
+++ pkg/raster/R/replacement.R
@@ -25,17 +25,33 @@ setReplaceMethod("[", c("RasterLayer", "ANY", "missing", "ANY"),
 	}
 )
 
+
+setMethod("[", "RasterStack",
+	function(x,i,j,layer,...,drop=FALSE) {
+		if  (!missing(layer)) {	stop("incorrect number of dimensions") }
+		if  (missing(j)) {	return(values(x)[i, ]) 
+		} else {
+			v <- valuesRow(x,j)
+			return(v[i])
+		}
+	}
+)
+
+
 setMethod("[", "RasterLayer",
-	function(x,i,j,...,drop=FALSE) {
-# if ...
-		return(values(x)[i])
+	function(x,i,j,layer,...,drop=FALSE) {
+		if  (!missing(layer)) {	stop("incorrect number of dimensions") }
+		if  (missing(j)) {	return(values(x)[i]) 
+		} else {
+			v <- valuesRow(x,j)
+			return(v[i])
+		}
 	}
 )
 
 
 setMethod("[[", c("RasterLayer", "ANY", "missing"), 
 	function(x, i, j, ...) {
-# if ...
 		return(values(x)[i])
 	}
 )
