af7583e70ed323f030a4d8345a659dc753e35c88
jacobvanetten (Dr ) <jacobvanetten@yahoo.com>
2009-01-27 13:10:16 +0000

    distance signature = RasterLayer added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@194 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/distance.R
+++ pkg/raster/R/distance.R
@@ -4,6 +4,10 @@
 # Version 0,7
 # Licence GPL v3
 
+#should become methods of generic "distance"
+#... could become the method to distinguish between Euclidean and great circle distances. 
+#Methods should be defined for four column matrices, two column matrices (all combinations, resulting in a dist object) and SpatialPoints 
+
 distanceEuclidean <- function (point1, point2) {
 	if (length(point1) == 2) {
 		x1 <- point1[1]
@@ -53,3 +57,53 @@ distanceGreatcircle <- function (point1, point2, r=6378137) {
 	return(distance)
 }
 
+# Author: Jacob van Etten jacobvanetten@yahoo.com
+# International Rice Research Institute
+# Date :  January 2009
+# Version 1.0
+# Licence GPL v3
+
+setGeneric("distance", function(object, ...) standardGeneric("distance"))
+
+setMethod("distance", signature(object = "RasterLayer"), def = function(object)
+	{
+		n <- ncell(object)
+		fromCells <- which(!is.na(values(object)))
+		toCells <- which(is.na(values(object)))
+		accDist <- rep(0,times=n)
+		if (isLatLon(object))
+		{
+			while(length(toCells)>0)
+			{			
+				adj <- adjacency(object,fromCells=fromCells,toCells=toCells,directions=8)
+				coord <- cbind(xyFromCell(object,adj[,1]),xyFromCell(object,adj[,2]))
+				distance <- apply(coord,1,function(x){distanceGreatcircle(x[1:2],x[3:4])})
+				transitionValues <- accDist[adj[,1]] + distance
+				transitionValues <- tapply(transitionValues,adj[,2],min)
+				transitionValues <- transitionValues[transitionValues < Inf]
+				fromCells <- as.integer(names(transitionValues))
+				accDist[fromCells] <- transitionValues 
+				toCells <- toCells[!(toCells %in% fromCells)]
+			}
+		}
+		if (!(isLatLon(object)))
+		{
+			while(length(toCells)>0)
+			{			
+				adj1 <- adjacency(object,fromCells=fromCells,toCells=toCells,directions=4)
+				adj2 <- adjacency(object,fromCells=fromCells,toCells=toCells,directions="Bishop")
+				distance <- c(rep(1,length=length(adj1[,1])),rep(sqrt(2),length=length(adj1[,1])))
+				adj <- rbind(adj1,adj2)
+				transitionValues <- accDist[adj[,1]] + distance
+				transitionValues <- tapply(transitionValues,adj[,2],min)
+				transitionValues <- transitionValues[transitionValues < Inf]
+				fromCells <- as.integer(names(transitionValues))
+				accDist[fromCells] <- transitionValues 
+				toCells <- toCells[!(toCells %in% fromCells)]
+			}
+		}
+		result <- object
+		result <- setValues(result, accDist)	
+		return(result)
+	}
+)
