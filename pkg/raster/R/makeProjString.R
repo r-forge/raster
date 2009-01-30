a33a526c7e90c79e2d19367fb32d6cc5016e2412
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-01-30 05:51:03 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@218 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/raster/R/makeProjString.R
@@ -0,0 +1,57 @@
+# Author: Robert J. Hijmans, r.hijmans@gmail.com
+# International Rice Research Institute
+# Date :  January 2009
+# Version 0.8
+# Licence GPL v3
+
+
+
+makeProj <- function(projection='longlat', ..., ellipsoid="", datum="WGS84", asText=TRUE) {
+	prj <- projInfo("proj")
+	ell <- projInfo("ellps")
+	dat <- projInfo("datum")
+	projection <- trim(projection)
+	ellipsoid <- trim(ellipsoid)
+	datum <- trim(datum)
+	if (!(projection %in% prj[,1])) {
+		stop("unknown projection. See projInfo()") 
+	} else {
+		pstr <- paste('+proj=',projection, sep="")
+		projname <- as.vector(prj[which(prj[,1]==projection), 2])
+	}
+	pargs <- list(...)
+	for (i in 1:length(pargs)) {
+		pstr <- paste(pstr, ' +', pargs[[i]], sep="")
+	}
+	if (ellipsoid != "") {
+		if (!(ellipsoid %in% ell[,1])) { 
+			stop("unknown ellipsoid. See projInfo('ellps')") 
+		} else {
+			pstr <- paste(pstr, " +ellps=", ellipsoid, sep="")
+#			ellipname <- ell[which(ell[,1]==ellipsoid), 2]
+		}
+	}
+	if (datum != "") {
+		if (!(datum %in% dat[,1])) { 
+			stop("unknown datum. See projInfo('datum')") 
+		} else {
+			pstr <- paste(pstr, " +datum=", datum, sep="")
+#			datumname <- as.vector(dat[which(dat[,1]==datum), 2])
+		}
+	}
+	
+	cat("Projection: ", projname[1], "\n")
+	
+	crs <- newCRS(pstr)
+
+		
+	if (asText) { 
+		return(trim(crs@projargs))
+	} else {
+		return(crs)
+	}
+}
+
+ #newprj2 <- makeProj("lcc", "lat_1=48", "lat_2=33", "lon_0=-100", asText=T)
+
+
