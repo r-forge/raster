6d22169757a6b8cc091f87e82eacb710b3fdd095
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-02-22 03:02:58 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@288 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/raster/R/rasterToPoints.R
@@ -0,0 +1,48 @@
+# Author: Robert J. Hijmans, r.hijmans@gmail.com
+# International Rice Research Institute
+# Date :  February 2009
+# Version 0.8
+# Licence GPL v3
+
+
+
+rasterToPoints <- function(raster, rm.na=TRUE, fun=NULL, asSpatialPoints=FALSE) {
+	if (dataContent(raster) == 'all') {
+		v <- values(raster)
+		raster <- clearValues(raster)
+		xyv <- cbind(xyFromCell(raster, 1:ncell(raster)), v)
+		if (rm.na) {
+			xyv <- subset(xyv, !(is.na(xyv[,3])))
+		}
+		if (!is.null(fun)) {
+			xyv <- subset(xyv, fun(xyv[,3]))
+		}
+	} else {
+		if (dataSource == 'ram') {
+			return(xyFromCell(raster, 1:ncell(raster)))
+		} else {
+			xyv <- matrix(NA, ncol=3, nrow=0)
+			colnames(xyv) <- c('x', 'y', 'v')
+			x <- xFromCol(raster, 1:ncol(raster))
+			for (r in 1:nrow(raster)) {
+				y <- yFromRow(raster, r)
+				raster <- readRow(raster, r)
+				xyvr <- cbind(x, y, values(raster))
+				if (rm.na) {
+					xyvr <- subset(xyvr, !(is.na(xyvr[,3])))
+				}
+				if (!is.null(fun)) {
+					xyvr <- subset(xyvr, fun(xyvr[,3]))
+				}
+				xyv <- rbind(xyv, xyvr)
+			}
+		}
+	}
+	if (asSpatialPoints) {
+		coords <- xyv[,1:2]
+		row.names(coords) <- 1:nrow(coords)
+		return(SpatialPointsDataFrame(coords=coords, data=as.data.frame(xyv[,3]), proj4string=projection(raster, asText=F)))
+	} else {
+		return(xyv)
+	}
+}
