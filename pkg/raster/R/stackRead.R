b550ee5e3f9c5c6844d98b941f26ba2940cef7ad
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-03-13 09:06:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@349 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/stackRead.R
+++ pkg/raster/R/stackRead.R
@@ -28,29 +28,6 @@
 
 
 
-.stackReadXY <- function(object, xyCoords, method='simple') {
-
-	if (method == 'bilinear') {
-		for (i in seq(nlayers(object))) {
-			v <- .bilinearValue(object, xyCoords)
-			if (i == 1) {
-				result <- v
-			} else {
-				result <- cbind(result, v)
-			}
-		}
-		if (!(is.null(dim(result)))) {
-			colnames(result) <- object@data@colnames
-		}	
-		return(result)		
-	
-	} else {
-		cells <- cellFromXY(object, xyCoords)
-		return(.stackReadCells(object, cells))
-	}
-	
-}
-
 
 .stackReadCells <- function(object, cells) {
 		for (i in seq(nlayers(object))) {
@@ -67,4 +44,5 @@
 		}	
 		return(result)
 }
+
 
--- pkg/raster/R/xyValues.R
+++ pkg/raster/R/xyValues.R
@@ -31,7 +31,26 @@ setMethod("xyValues", signature(object='RasterStack', xyCoords='matrix'),
 		if (dim(xyCoords)[2] != 2) {
 			stop('xyCoords has wrong dimensions; there should be 2 columns only' )
 		}
-		return(.stackReadXT(object, cells, method))
+		
+
+		if (method == 'bilinear') {
+			for (i in seq(nlayers(object))) {
+				v <- .bilinearValue(object, xyCoords)
+				if (i == 1) {
+					result <- v
+				} else {
+					result <- cbind(result, v)
+				}
+			}
+			if (!(is.null(dim(result)))) {
+				colnames(result) <- object@data@colnames
+			}	
+			return(result)		
+	
+		} else {
+			cells <- cellFromXY(object, xyCoords)
+			return(.stackReadCells(object, cells))
+		}
 	}	
 )
 
