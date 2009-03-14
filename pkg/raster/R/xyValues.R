6d6d44d6ae5687ffaa04850c701c3de3cdcff4bc
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-03-14 08:06:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@358 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/xyValues.R
+++ pkg/raster/R/xyValues.R
@@ -10,6 +10,25 @@ if (!isGeneric("xyValues")) {
 	setGeneric("xyValues", function(object, xyCoords, ...)
 		standardGeneric("xyValues"))
 }	
+
+
+setMethod("xyValues", signature(object='Raster', xyCoords='SpatialPoints'), 
+	function(object, xyCoords, method='simple') { 
+		xyCoords <- coordinates(xyCoords)
+		callNextMethod(object, xyCoords, method=method)
+	}	
+)
+
+
+setMethod("xyValues", signature(object='Raster', xyCoords='vector'), 
+	function(object, xyCoords, method='simple') { 
+		if (length(xyCoords) != 2) {
+			stop('xyCoords should be a two column matrix or a vector of length 2')
+		}
+		xyCoords <- matrix(xyCoords, ncol=2)
+		callNextMethod(object, xyCoords, method=method)
+	}
+)
 	
 setMethod("xyValues", signature(object='RasterLayer', xyCoords='matrix'), 
 	function(object, xyCoords, method='simple') { 
@@ -18,9 +37,11 @@ setMethod("xyValues", signature(object='RasterLayer', xyCoords='matrix'),
 		}
 		if (method=='bilinear') {
 			return(.bilinearValue(object, xyCoords))
-		} else {
+		} else if (method=='simple') {
 			cells <- cellFromXY(object, xyCoords)
 			return(.rasterReadCells(object, cells))
+		} else {
+			stop('invalid method argument. Should be simple or bilinear.')
 		}
 	}	
 )	
@@ -35,7 +56,8 @@ setMethod("xyValues", signature(object='RasterStack', xyCoords='matrix'),
 
 		if (method == 'bilinear') {
 			for (i in seq(nlayers(object))) {
-				v <- .bilinearValue(object, xyCoords)
+				r <- asRasterLayer(object, i)
+				v <- .bilinearValue(r, xyCoords)
 				if (i == 1) {
 					result <- v
 				} else {
@@ -47,27 +69,16 @@ setMethod("xyValues", signature(object='RasterStack', xyCoords='matrix'),
 			}	
 			return(result)		
 	
-		} else {
+		} else if (method==simple) {
 			cells <- cellFromXY(object, xyCoords)
 			return(.stackReadCells(object, cells))
+		} else {
+			stop('invalid method argument. Should be simple or bilinear.')
 		}
+
 	}	
 )
 
 
 
-setMethod("xyValues", signature(object='RasterLayer', xyCoords='SpatialPoints'), 
-	function(object, xyCoords, method='simple') { 
-		xyCoords <- coordinates(xyCoords)
-		callNextMethod(object, xyCoords, method)
-	}	
-)
-
-
-setMethod("xyValues", signature(object='RasterStack', xyCoords='SpatialPoints'), 
-	function(object, xyCoords, method='simple') { 
-		xyCoords <- coordinates(xyCoords)
-		callNextMethod(object, xyCoords, method)
-	}	
-)
 
