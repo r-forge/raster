4618cfdc6ecc4c1310572285ca49e0ea15fdb3bd
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-04-05 15:30:47 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@393 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/writeGDAL.R
+++ pkg/raster/R/writeGDAL.R
@@ -62,10 +62,27 @@
 	if ( rownr == 1) {
 		transient <- .getGDALtransient(raster, gdalfiletype, overwrite, mvFlag, options)
 		attr(raster@file, "transient") <- transient
-		
 #		raster@file@driver <- 'gdal'
 		raster@data@source <- 'disk'		
 	}	
+	
+#	raster@data@values[is.nan(raster@data@values)] <- NA
+#	raster@data@values[is.infinite(raster@data@values)] <- NA
+#	if (raster@file@dtype == "INT" || raster@file@dtype =='LOG' ) { 
+#		values <- as.integer(round(raster@data@values))  
+#		values[is.na(values)] <- as.integer(raster@file@nodatavalue)		
+#	} else { 
+#		values  <- as.numeric( raster@data@values ) 
+#	}
+	
+	if (!raster@data@haveminmax) {
+		rsd <- na.omit(raster@data@values) # min and max values
+		if (length(rsd) > 0) {
+			raster@data@min <- min(raster@data@min, min(rsd))
+			raster@data@max <- max(raster@data@max, max(rsd))
+		}	
+	}
+	
     for (band in 1:nlayers(raster)) {
 		x <- putRasterData(raster@file@transient, values(raster, rownr), band, c((rownr-1), 0)) 
 	}
@@ -76,9 +93,12 @@
 		# establish the handle:
 		rasterout <- raster(filename(raster))
 		
-		rasterout@data@haveminmax <- raster@data@haveminmax
-		rasterout@data@min <- raster@data@min
-		rasterout@data@max <- raster@data@max
+		if (!raster@data@haveminmax) {
+			rasterout@data@min <- raster@data@min
+			rasterout@data@max <- raster@data@max
+		}
+		rasterout@data@haveminmax <- TRUE
+		
 		.writeStx(rasterout) 
 		return(rasterout)
 	}
