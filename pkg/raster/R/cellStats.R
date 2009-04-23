536ca294a99daad5691f310d85deb5554f35a436
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-04-23 12:07:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@427 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/cellStats.R
+++ pkg/raster/R/cellStats.R
@@ -1,34 +1,65 @@
 # Author: Robert J. Hijmans, r.hijmans@gmail.com
 # International Rice Research Institute
-# Date : June 2008
+# Date : March 2009
 # Version 0.8
 # Licence GPL v3
 
-.Old.cellStats <- function(x, ..., na.rm=TRUE) {
-	funs <- list(...)
-	if (length(funs) == 0) {
-		stop('you must provide a function as argument')
-	}
-	res <- list()
-	if (dataContent(x) != 'all') {
-		if (dataSource(x) == 'ram') {
-			stop('no values associated with this RasterLayer')
-		}
-		if (canProcessInMemory(x, 2)) {
-			x <- readAll(x)
-		}
-	}
-	if (dataContent(x) == 'all') {
-		for(i in seq(along=funs)) {
-			if (na.rm) {
-				res[i] <- funs[[i]](na.omit(x@data@values))
-			} else {
-				res[i] <- funs[[i]](x@data@values)
+cellStats <- function(raster, stat='mean', track=-1) {
+
+	if (class(stat) != 'character') {
+		if (dataContent(raster) == 'all') { n <- 1 } else {n <- 2}
+		if (canProcessInMemory(raster, n)) {
+			if (dataContent(raster) != 'all') {
+				raster <- readAll(raster)
 			}
+			d <- na.omit(values(raster))
+			return( stat(d) )
+		} else {
+			stop("RasterLayer is too large. You can use fun='sum', 'mean', 'min', or 'max', but not a function")
 		}
 	} else {
-		stop('sorry, only implemented for rasters that can be loaded into memory')
+
+		counts <- FALSE
+		if (stat == 'sum') {
+			fun <- sum
+		} else if (stat == 'min') {
+			fun <- min
+		} else if (stat == 'max') {
+			fun <- max
+		} else if (stat == 'mean' | stat == 'sd') {
+			# do nothing
+		} else { 
+			stop("invalid 'stat', should be 'sum', 'min', 'max', 'sd' or 'mean'") 
+		}
+
+		cnt <- 0
+		sumsq <- 0
+		st  <- NULL
+		starttime <- proc.time()
+		for (r in 1:nrow(raster)) {
+			d <- na.omit(valuesRow(raster, r))
+			if (length(d) == 0) { next }
+			if (stat == 'sd') {
+				st <- sum(d, st)
+				cnt <- cnt + length(d)
+				sumsq <- sum(d^2, sumsq)
+			} else if (stat=='mean') {
+				st <- sum(d, st)
+				cnt <- cnt + length(d)
+			} else {
+				st <- fun(c(d, st))
+			}
+			if (r %in% track) { 
+				.showTrack(r, raster@nrows, track, starttime) 
+			}
+		}
+		if (stat == 'sd') {
+			meansq <- (st/cnt)^2
+			st <- sqrt( (1 / cnt) * sumsq - meansq )
+		} else if (stat == 'mean') {
+			st <- st / cnt
+		}
+		return(st)
 	}
-	return(unlist(res))
 }
-	
+
