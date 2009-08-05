5a2b06b9420f6eae2cf0133f4ff6229551625ece
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-08-05 00:46:22 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@465 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/predict.R
+++ pkg/raster/R/predict.R
@@ -5,14 +5,28 @@ if (!isGeneric("predict")) {
 }	
 
 setMethod('predict', signature(object='RasterStack'), 
-	function(object, model, filename="", datatype='FLT4S', filetype = 'raster', overwrite=FALSE, track=-1, ...) {
+	function(object, model, filename="", datatype='FLT4S', filetype = 'raster', overwrite=FALSE, track=-1, vartype=NA, ...) {
 		predrast <- raster(object)
 		filename(predrast) <- filename
 		dataType(predrast) <- datatype
 		
+		dataclasses <- attr(model$terms, "dataClasses")
+		f <- which(dataclasses == 'factor')
+		if (length(f) > 0) { 
+			haveFactor <- TRUE 
+		} else {
+			haveFactor <- FALSE
+		}
+		
 		if (dataContent(object) == 'all') {
 			indices <- 1:ncell(predrast)
 			rowvals <- data.frame( na.omit(cbind( indices, values(object, names=TRUE)) ) )
+			if (haveFactor) {
+				for (i in 1:length(f)) {
+					rowvals[,f[i]] <- as.factor(rowvals[,f[i]])
+				}
+			}
+			
 			predv <- indices
 			predv[] <- NA
 			predv[indices] <- predict(model, rowvals[,-1], ...)
@@ -26,7 +40,14 @@ setMethod('predict', signature(object='RasterStack'),
 			predv <- 1:ncol(object)
 			for (r in 1:nrow(object)) {
 				object <- readRow(object, r)
+				
 				rowvals <- na.omit( cbind(myIdVar, values(object, names=TRUE)) )
+				if (haveFactor) {
+					for (i in 1:length(f)) {
+						rowvals[,f[i]] <- as.factor(rowvals[,f[i]])
+					}
+				}
+
 				indices <- rowvals[,1]
 				predv[] <- NA
 				if (length(indices) > 0) {
