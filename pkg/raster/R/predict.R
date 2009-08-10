3d4f6762485ddf65fb352c3b1d9c3aa104d83816
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-08-10 17:48:32 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@468 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/predict.R
+++ pkg/raster/R/predict.R
@@ -1,3 +1,8 @@
+# Author: Robert J. Hijmans, r.hijmans@gmail.com
+# University of California, Davis
+# Date :  August 2009
+# Version 0.8
+# Licence GPL v3
 
 if (!isGeneric("predict")) {
 	setGeneric("predict", function(object, ...)
@@ -19,40 +24,29 @@ setMethod('predict', signature(object='RasterStack'),
 		}
 		
 		if (dataContent(object) == 'all') {
-			indices <- 1:ncell(predrast)
-			rowvals <- data.frame( na.omit(cbind( indices, values(object, names=TRUE)) ) )
+			rowvals <- data.frame( values(object, names=TRUE))
 			if (haveFactor) {
 				for (i in 1:length(f)) {
 					rowvals[,f[i]] <- as.factor(rowvals[,f[i]])
 				}
 			}
 			
-			predv <- indices
-			predv[] <- NA
-			predv[indices] <- predict(model, rowvals[,-1], ...)
+			predv <- predict(model, rowvals, ...)
 			predrast <- setValues(predrast, predv)
 			if (filename(predrast) != "") {
 				predrast <- writeRaster(predrast)
 			}
 			return(predrast)
 		} else {
-			myIdVar <- 1:ncol(object)
-			predv <- 1:ncol(object)
 			for (r in 1:nrow(object)) {
 				object <- readRow(object, r)
-				
-				rowvals <- na.omit( cbind(myIdVar, values(object, names=TRUE)) )
+				rowvals <- data.frame( values(object, names=TRUE))		
 				if (haveFactor) {
 					for (i in 1:length(f)) {
 						rowvals[,f[i]] <- as.factor(rowvals[,f[i]])
 					}
 				}
-
-				indices <- rowvals[,1]
-				predv[] <- NA
-				if (length(indices) > 0) {
-					predv[indices] <- predict(model, data.frame(rowvals[,-1]), ...)
-				}
+				predv <- predict(model, rowvals, ...)
 				predrast <- setValues(predrast, predv, r)
 				predrast <- writeRaster(predrast, filetype=filetype, overwrite=overwrite)
 			}
