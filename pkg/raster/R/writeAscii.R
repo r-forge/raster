128a4d94e3bee7a20796adfb640cb36c981d683c
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-09-10 06:07:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@488 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/writeAscii.R
+++ pkg/raster/R/writeAscii.R
@@ -14,9 +14,9 @@
 	
 	if (dataIndices(raster)[1] == 1) {
 		resdif <- abs((yres(raster) - xres(raster)) / yres(raster) )
-		if (resdif > 0.001) {
+		if (resdif > 0.01) {
 			stop(paste("raster has unequal horizontal and vertical resolutions","\n", "these data cannot be stored in arc-ascii format"))
-		} else if (resdif > 0.00001) {
+		} else if (resdif > 0.001) {
 			warning("arc-ascii format ignore that this raster has slightly unequal horizontal and vertical resolutions")
 		}
 		if (!overwrite & file.exists(filename)) {
