a1bd2374028c2474b01280281ff021e5f0810245
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-10-02 00:51:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@549 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/drawPoly.R
+++ pkg/raster/R/drawPoly.R
@@ -1,28 +1,26 @@
 # R function for the raster package
 # Author: Robert J. Hijmans
-# International Rice Research Institute. Philippines
 # contact: r.hijmans@gmail.com
 # Date : January 2009
-# Version 0.8
+# Version 0.9
 # Licence GPL v3
 
 
-drawPoly <- function(sp=TRUE, col='red') {
-	xy <- locator(n=10000, type="l", col=col)
+drawPoly <- function(sp=TRUE, col='red', lwd=2, ...) {
+	xy <- locator(n=10000, type="l", col=col, lwd=lwd, ...)
 	xy <- cbind(xy$x, xy$y)
 	xy <- rbind(xy, xy[1,])
-	lines(xy[(length(xy[,1])-1):length(xy[,1]),], col=col)
+	lines(xy[(length(xy[,1])-1):length(xy[,1]),], col=col, lwd=lwd, ...)
 	if (sp) {
 		return( SpatialPolygons(list(Polygons(list(Polygon(xy)), 1))) )
 	} else {
 		return(xy)
 	}
-	
 }
 
 
-drawLine <- function(sp=TRUE, col='red') {
-	xy <- locator(n=10000, type="l", col=col)
+drawLine <- function(sp=TRUE, col='red', lwd=2, ...) {
+	xy <- locator(n=10000, type="l", col=col, lwd=lwd, ...)
 	xy <- cbind(xy$x, xy$y)
 	if (sp) {
 		return( SpatialLines(list(Lines(list(Line(xy)), "1"))) )
