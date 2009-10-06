b5fdb3cff7ceae59fa8c6279fb4de16be6edb83a
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-10-06 16:16:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@552 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/calcStack.R
+++ pkg/raster/R/calcStack.R
@@ -1,7 +1,6 @@
 # Author: Robert J. Hijmans, r.hijmans@gmail.com
-# International Rice Research Institute
 # Date :  June 2008
-# Version 0.8
+# Version 0.9
 # Licence GPL v3
 
 
@@ -22,35 +21,29 @@ function(x, fun, filename="", ...) {
 	filename <- trim(filename)
 	outraster <- raster(x, filename)
 	dataType(outraster) <- datatype
-	if (dataContent(x) == "all") {
-		outraster <- setValues(outraster, apply(values(x), 1, fun)) 
-		if (filename != "") {
+
+	if (!canProcessInMemory(x, 4) & filename == '') {
+		filename=rasterTmpFile()
+		filename(outraster) <- filename
+	}
+	v <- vector(length=0)
+
+	starttime <- proc.time()
+	pb <- .setProgressBar(nrow(x), type=.progress(...))
+	for (r in 1:nrow(x)) {
+		sv <- apply(getValues(x, r), 1, fun)
+		if (outraster@file@name == "") {
+			v <- c(v, sv)
+		} else {
+			outraster <- setValues(outraster, sv, r) 
 			outraster <- writeRaster(outraster, filetype=filetype, overwrite=overwrite)
 		}
-	} else {
-		if (!canProcessInMemory(x, 4) & filename == '') {
-			filename=rasterTmpFile()
-			filename(outraster) <- filename
-		}
-		v <- vector(length=0)
-
-		starttime <- proc.time()
-		pb <- .setProgressBar(nrow(x), type=.progress(...))
-		for (r in 1:nrow(x)) {
-			x <- readRow(x, r)
-			if (outraster@file@name == "") {
-				v <- c(v, apply(values(x), 1, fun))
-			} else {
-				outraster <- setValues(outraster, apply(values(x), 1, fun), r) 
-				outraster <- writeRaster(outraster, filetype=filetype, overwrite=overwrite)
-			}
-			.doProgressBar(pb, r) 
-		}
-		.closeProgressBar(pb, starttime)
-		if (outraster@file@name == "") { 
-			outraster <- setValues(outraster, v) 
-		}
-	}		
+		.doProgressBar(pb, r) 
+	}
+	.closeProgressBar(pb, starttime)
+	if (outraster@file@name == "") { 
+		outraster <- setValues(outraster, v) 
+	}
 	return(outraster)
 }
 )
