a26d7bde32285fb4612e68471dfef4a587c0ac11
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-10-31 17:17:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@591 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/zzz.R
+++ pkg/raster/R/zzz.R
@@ -2,7 +2,7 @@
 .onLoad <- function(lib, pkg)  {
 	pkg.info <- drop(read.dcf(file=system.file("DESCRIPTION", package=pkg), fields=c("Version","Date")))
 	cat(paste("\n",pkg, " version ", pkg.info["Version"], " (", pkg.info["Date"], ")\n", sep=""))
-	removeTmpFiles()
+	tst <- try( removeTmpFiles(), silent=TRUE )
 	return(invisible(0))
 }
 
