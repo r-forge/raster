0eecc1549fe3dae8d63e3b65104b11e3b378045e
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-11-11 20:12:19 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@604 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/expand.R
+++ pkg/raster/R/expand.R
@@ -38,73 +38,49 @@ function(x, y, filename='', ...) {
 	bndbox <- newExtent(xmn, xmx, ymn, ymx)
 	outraster <- setExtent(outraster, bndbox, keepres=TRUE)
 
-	startrow <- rowFromY(outraster, ymax(x))
-	startcol <- colFromX(outraster, xmin(x))
-	
-	if ((dataContent(x) == 'all') | ( dataSource(x) == 'disk' ))  {
-		forcetodisk <- FALSE
-		if (!canProcessInMemory(outraster, 2) && filename == '') {
+	if (canProcessInMemory(outraster, 2)) {
+		if (dataContent(x) != 'all') { 
+			x <- readAll(x) 
+		}
+		v <- vector(length=ncol(outraster))
+		v[] <- NA		
+		datatype <- dataType(x)
+		d <- vector(length=ncell(outraster))
+		d[] <- NA
+		cells <- cellsFromExtent(outraster, extent(x))
+		d[cells] <- values(x)
+		outraster <- setValues(outraster, d)	
+		if (filename != '') {
+			outraster <- writeRaster(outraster, filename=filename, datatype=dataType(x), ...)
+		}
+	} else { 
+		if (filename == '') {
 			filename <- rasterTmpFile()
-			forcetodisk <- TRUE
 			if (getOption('verbose')) { cat('writing raster to:', filename)	}						
 		}
-	}
-	
-	if (dataContent(x) == 'all')  {
 
-		if (forcetodisk) {
-			v <- vector(length=ncol(outraster))
-			v[] <- NA		
-			datatype <- datatype(x)
-			for (r in 1:nrow(x)) {
-				vals <- getValues(x, r) 
-				if (todisk) {
-					vv <- v
-					vv[startcol:(startcol+ncol(x)-1)] <- vals
-					outraster <- setValues(outraster, vv, r)	
-					outraster <- writeRaster(outraster, filename=filename, datatype=datatype, ...)
-				}
-			}
-		} else {
-			d <- vector(length=ncell(outraster))
-			d[] <- NA
-			cells <- cellsFromExtent(outraster, extent(x))
-			d[cells] <- values(x)
-			outraster <- setValues(outraster, d)	
-			if (filename != '') {
-				outraster <- writeRaster(outraster, filename=filename, datatype=dataType(x), ...)
-			}
-		}
+		startrow <- rowFromY(outraster, yFromRow(x, 1))
+		endrow <- rowFromY(outraster, yFromRow(x, nrow(x)))
+		startcol <- colFromX(outraster, xFromCol(x, 1))
+		endcol <- colFromX(outraster, xFromCol(x, ncol(x)))
 		
-	} else if ( dataSource(x) == 'disk' ) { 
-				
-		pb <- pbCreate(nrow(x), type=.progress(...))
-
+		pb <- pbCreate(nrow(outraster), type=.progress(...))
 		v <- vector(length=0)
 		d <- vector(length=ncol(outraster))
-		datatype <- datatype(x)
-		for (r in 1:nrow(x)) {
-		
-			x <- readRow(x, r)
-			vals <- values(x)
+		datatype <- dataType(x)
+		xr <- 0
+		for (r in 1:nrow(outraster)) {
 			d[] <- NA
-			startcell <- (r + startrow -2) * ncol(outraster) + startcol
-			d[startcell:(startcell+ncol(x)-1)] <- vals
-
-			if (filename != '') {
-				outraster <- setValues(outraster, d, r)
-				outraster <- outraster <- writeRaster(outraster, filename=filename, datatype=datatype, ...)
-			} else {
-				v <- c(v, d)
+			if (r >= startrow & r <= endrow) {
+				xr <- xr + 1
+				vals <- getValues(x, xr)
+				d[startcol:endcol] <- vals
 			}
-
-			pbStep(pb, r)
+			outraster <- setValues(outraster, d, r)
+			outraster <- writeRaster(outraster, filename=filename, datatype=datatype, ...)
+			pbStep(pb, r) 			
 		}
 		pbClose(pb)
-
-		if (filename == "") { 
-			outraster <- setValues(outraster, v) 
-		}
 	} 
 	return(outraster)
 }
