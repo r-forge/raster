bc8345388b897f5417c0bc7d04947a4e4991de08
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-02-04 01:47:47 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@727 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/plot.R
+++ pkg/raster/R/plot.R
@@ -13,16 +13,10 @@ if (!isGeneric("plot")) {
 setMethod("plot", signature(x='RasterStackBrick', y='ANY'), 
 	function(x, y, col=rev(terrain.colors(255)), maxpixels=100000, ...)  {
 		if (missing(y)) {
-			nl <- nlayers(x)
-			if (nl > 12) {
-				warning('only first 12 layers are plotted')
-				nl <- 12
-			}
-			nc <- ceiling(sqrt(nl))
-			nr <- ceiling(nl / nc)
-			par(mfrow=c(nr, nc))
-			for (i in 1:nl) {	
-				.plotraster(raster(x, i), col=col, maxpixels=maxpixels, main=layerNames(x)[i],  ...) 
+			y <- 1:nlayers(x)
+			if (length(y) > 16) {
+				warning('only first 16 layers are plotted')
+				y <- 1:16
 			}
 		} else {
 			if (is.character(y)) {
@@ -33,15 +27,30 @@ setMethod("plot", signature(x='RasterStackBrick', y='ANY'),
 			}
 			y = na.omit(y)
 			y <- unique(as.integer(round(y)))
-			if (length(y) > 1) {
-				nl <- length(y)
-				nc <- ceiling(sqrt(nl))
-				nr <- ceiling(nl / nc)
-				par(mfrow=c(nr, nc))
-				par(mfrow=c(nr, nc))
-				for (i in 1:length(y)) {
-					.plotraster(raster(x, y[i]), col=col, maxpixels=maxpixels, main=layerNames(x)[y[i]], ...) 
+		}
+		if (length(y) == 1) {
+			.plotraster(raster(x, y), col=col, maxpixels=maxpixels, main=layerNames(x)[y], ...) 
+		} else {
+
+			nl <- length(y)
+			nc <- ceiling(sqrt(nl))
+			nr <- ceiling(nl / nc)
+				
+			old.par <- par(no.readonly = TRUE) 
+			on.exit(par(old.par))
+			par(mfrow=c(nr, nc))
+			xa='n'
+			rown=1
+			coln=0
+			for (i in 1:length(y)) {
+				coln = coln + 1
+				if (coln > nc) {
+					coln <- 1
+					rown = rown + 1
 				}
+				if (rown==nr) xa='s'
+				if (coln==1) ya='s' else ya='n'
+				.plotraster(raster(x, y[i]), col=col, maxpixels=maxpixels, xaxt=xa, yaxt=ya, main=layerNames(x)[y[i]], ...) 
 			}		
 		}
 	}
