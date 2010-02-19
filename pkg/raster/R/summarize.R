0eec64b077671d6080d44e8abbc1c24d38459644
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-02-19 18:28:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@787 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/summarize.R
+++ pkg/raster/R/summarize.R
@@ -4,59 +4,91 @@
 # Version 0.9
 # Licence GPL v3
 
+setMethod('summary', signature(object='RasterLayer'), 
+	function(object, ...) {
+		sumobj <- new("RasterSummary")
+		sumobj@ncell <- ncell(object)
+		sumobj@dataContent <- dataContent(object) 
+		if ( sumobj@dataContent == "all") {
+			sumobj@NAs <- sum(is.na(object@data@values))
+			sumobj@values <- as.matrix( summary(object@data@values) )
+			colnames(sumobj@values)=""
+		} 
+		return(sumobj)
+	}	
+)
+
 
 setMethod('summary', signature(object='RasterStack'), 
 	function(object, ...) {
-		cat ("Cells: " , ncell(object), "\n")
+		sumobj <- new("RasterSummary")
+		sumobj@ncell <- ncell(object)
 		for (n in 1:nlayers(object)) {
+			sumobj@dataContent <- c(sumobj@dataContent, dataContent(object@layers[[n]]) )
 			if (dataContent(object@layers[[n]]) == 'all') {
-				cat("layer ", n, "\n")
-				cat("   NAs : ", sum(is.na(values(object@layers[[n]]))), "\n")
-				summary(values(object@layers[[n]]))
+				sumobj@NAs <- c(sumobj@NAs, sum(is.na(object@layers[[n]]@data@values)))
+				sm = as.matrix( summary(object@layers[[n]]@data@values) )
+				sumobj@values <- cbind(sumobj@values, as.matrix(sm))
+				rownames(sumobj@values) <- rownames(sm)
 			} else {
-				cat("layer ", n, "\n")
-				cat("   values not in memory\n")
+				sumobj@NAs <- c(sumobj@NAs, NA)
+				sumobj@values <- cbind(sumobj@values , NA)
 			}
 		}
+		colnames(sumobj@values) <- 1:nlayers(object)
+		return(sumobj)
 	}
 )	
 
 
-setClass('RasterLayerSummary',
+
+
+setMethod('summary', signature(object='RasterBrick'), 
+	function(object, ...) {
+		sumobj <- new("RasterSummary")
+		sumobj@ncell <- ncell(object)
+		sumobj@dataContent <- dataContent(object)
+
+		if (dataContent(object) == 'all') {
+			for (n in 1:nlayers(object)) {
+				sumobj@NAs <- c(sumobj@NAs, sum(is.na(object@data@values[, n])))
+				sm = as.matrix( summary( object@data@values[,n] ) )
+				sumobj@values <- cbind(sumobj@values, as.matrix(sm))
+				rownames(sumobj@values) <- rownames(sm)
+			} 
+			colnames(sumobj@values) <- 1:nlayers(object)
+		}
+		return(sumobj)
+	}
+)	
+
+
+setClass('RasterSummary',
 	representation (
 		ncell = 'numeric',
-		dataContent = 'character',
-		NAs = 'numeric',
+		dataContent = 'vector',
+		NAs = 'vector',
 		values = 'matrix'
-	)
+	),
+	prototype (	
+		dataContent = vector(mode='character'),
+		NAs = vector(mode='numeric'),
+		values = matrix(nrow=6, ncol=0)
+	),
 )
 	
 
-setMethod('show', signature(object='RasterLayerSummary'), 	
+setMethod('show', signature(object='RasterSummary'), 	
 	function(object) {
 		cat ("Cells: " , object@ncell, "\n")
-		if ( object@dataContent == "all") {
+		if ( any(object@dataContent=="all")) {
 			cat("NAs  : ", object@NAs, "\n")
-			cat("\nValues")
-			tab <- as.table(object@values) 
-			colnames(tab) <- ""
-			print(tab)
+			cat("\n")
+			print(object@values) 
 		} else {
 			cat("values not in memory\n")
 		}
 	}	
 )
 	
-setMethod('summary', signature(object='RasterLayer'), 
-	function(object, ...) {
-		sumobj <- new("RasterLayerSummary")
-		sumobj@ncell <- ncell(object)
-		sumobj@dataContent <- dataContent(object) 
-		if ( sumobj@dataContent == "all") {
-			sumobj@NAs <- sum(is.na(values(object)))
-			sumobj@values <- as.matrix( summary(values(object)) )
-		} 
-		return(sumobj)
-	}	
-)
 
