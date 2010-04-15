f969553b83acc1df21ef276ea62dae7984531e9f
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-04-15 20:43:33 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@907 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/plotRGB.R
+++ pkg/raster/R/plotRGB.R
@@ -15,16 +15,17 @@ function(x, r=1, g=2, b=3, maxpixels=100000, ...) {
 	r <- sampleRegular(raster(x,r), maxpixels, asRaster=TRUE)
 	g <- sampleRegular(raster(x,g), maxpixels, asRaster=TRUE)
 	b <- sampleRegular(raster(x,b), maxpixels, asRaster=TRUE)
-	rm(x)
+
 	x <- xFromCol(r, 1:ncol(r))
 	y <- yFromRow(r, nrow(r):1)
+
 	z <- rgb(getValues(r), getValues(g), getValues(b), max=255)
 	col <- unique(z)
 	z <- match(z, col)
 	z <- matrix(z, nrow=nrow(r), ncol=ncol(r), byrow=T)
 	z <- t(z[nrow(z):1,])
+
 	image(x=x, y=y, z=z, col=col, ...)
 }
 )
-
 
