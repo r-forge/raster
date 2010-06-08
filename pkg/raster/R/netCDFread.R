f8a005610029ed7ef3b49eaec599eeaa11b7b422
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-06-08 18:09:02 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@985 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/netCDFread.R
+++ pkg/raster/R/netCDFread.R
@@ -9,7 +9,7 @@
 	zvar = x@data@zvar
 	
 	if (x@file@nbands == 1) {
-		d <- as.vector(var.get.nc(nc, variable=zvar))
+		d <- var.get.nc(nc, variable=zvar)
 	} else {
 		time <- x@data@band
 		start <- c(1, 1, time)
@@ -25,12 +25,14 @@
 	}
 	d <- x@data@add_offset + d * x@data@scale_factor
 
-	if (! x@file@toptobottom ) { 
-		d <- d[nrow(x):1,] 	
+	if ( x@file@toptobottom ) { 
+		# in d, cols and rows are reversed, hence chaning the col order
+		d <- d[, ncol(d):1] 
 	}
-		
+	
 	return( as.vector(d) )
 }
+
 
 
 	
@@ -52,18 +54,26 @@
 	}
 	d <- x@data@add_offset + d * x@data@scale_factor
 	
-	if (! x@file@toptobottom ) { 
-		d <- d[nrow(x):1,] 	
+	if ( x@file@toptobottom ) { 
+		d <- d[, ncol(d):1] 	
 	}
 		
 	return( as.vector(d) )
 }
 
+
 	
 
 .brickGetValuesAllCDF <- function(x, time=1, ntimes=nlayers(x)-time+1) {	
+
+	time   =  min( max( round(time), 1), nlayers(x))
+	ntimes =  min( max( round(ntimes), 1), nlayers(x)-time+1 )
+	
 	nc <- open.nc(x@file@name)
-	d <- var.get.nc(nc, x@data@zvar)
+
+	start = c(1, 1, time)
+	count = c(x@ncols, x@nrows, ntimes)
+	d <- var.get.nc(nc, variable=x@data@zvar, start=start, count=count)
     close.nc(nc)
 
 	if (!is.na(x@file@nodatavalue)) {
@@ -73,22 +83,33 @@
 		d <- x@data@add_offset + d * x@data@scale_factor
 	}
 	
-	values <- matrix(nrow=ncell(x), ncol=nlayers(x))
-	for (i in 1:nlayers(x)) {
-		x <- t(d[,,i])
-		x <- x[nrow(x):1, ]
-		values[,i] <- as.vector(t(x))
+	values <- matrix(nrow=ncell(x), ncol=ntimes)
+	colnames(values) = time:(time+ntimes-1)
+	
+	if ( x@file@toptobottom ) { 
+		for (i in 1:ntimes) {
+			x <- (d[,,i])
+			values[,i] <- as.vector( x[, ncol(x):1] )
+		}
+	} else {
+		for (i in 1:ntimes) {
+			values[,i] <- as.vector(x)
+		}
 	}
+	
 	return(values)
 }	
 
 
 	
 .readRowsBrickNetCDF <- function(x, row, nrows=1, col=1, ncols=(ncol(x)-col+1), time=1, ntimes=nlayers(x)-time+1) {
-
-	if (! x@file@toptobottom ) { 
-		row <- x@nrows - row + 1
+	
+	if ( x@file@toptobottom )	{
+		row <- x@nrows - row + 1	
 	}
+	
+	time   =  min( max( round(time), 1), nlayers(x))
+	ntimes =  min( max( round(ntimes), 1), nlayers(x)-time+1 )
 	
 	nc <- open.nc(x@file@name)
 	zvar = x@data@zvar
@@ -102,21 +123,22 @@
 	}
 	d <- x@data@add_offset + d * x@data@scale_factor
 	
-	values <- matrix(nrow=nrows*ncol(x), ncol=nlayers(x))
-	if (nrows > 1) {
-		for (i in 1:nlayers(x)) {
+	values <- matrix(nrow=nrows*ncols, ncol=ntimes)
+	
+	if (length(dim(d)) == 3) {
+		for (i in 1:ntimes) {
 			values[,i] <- as.vector(d[,,i])
 		}
+		
 	} else {
-		for (i in 1:nlayers(x)) {
+		for (i in 1:ntimes) {
 			values[,i] <- as.vector(d[,i])
 		}
+		
 	}
 	return(values)
 }
-	
-	
-	
+
 
 #f = "G:/cmip/ipcc/20c3m/atm/mo/pr/bccr_bcm2_0/run1/pr_A1_1.nc"
 #r = .rasterFromCDF(f, zvar='pr', type='RasterLayer', time=1)
@@ -124,5 +146,3 @@
 
 #b = .rasterFromCDF(f, zvar='pr', type='RasterBr', time=10)
 #bd = .brickReadAllCDF(b)
-
-	
