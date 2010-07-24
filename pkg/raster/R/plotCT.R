833032087b696a34987b348efe938dd0c49b0f8d
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-07-24 23:48:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1040 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/raster/R/plotCT.R
@@ -0,0 +1,49 @@
+# Author: Robert J. Hijmans, r.hijmans@gmail.com
+# Date :  July 2010
+# Version 0.9
+# Licence GPL v3
+
+
+if (!isGeneric("plotCT")) {
+	setGeneric("plotCT", function(x, ...)
+		standardGeneric("plotCT"))
+}	
+
+setMethod("plotCT", signature(x='RasterLayer'), 
+function(x, coltab=NULL, maxpixels=100000, extent=NULL, axes=TRUE, xlab='', ylab='', asp, interpolate=FALSE, ...) { 
+	
+ 	if (missing(asp)) {
+		if (.couldBeLonLat(x)) {
+			ym <- mean(x@extent@ymax + x@extent@ymin)
+			asp <- min(5, 1/cos((ym * pi)/180))
+			asp = NA
+		} else {
+			asp = 1
+		}		
+	}
+
+
+	if (is.null(coltab) &  fromDisk(x) ) {
+		if (.requireRgdal()) {
+			coltab <- getColorTable( GDAL.open(filename(x)) )
+			if (length(coltab) == 0) coltab <- NULL
+		}
+	}
+
+	r <- sampleRegular(x, maxpixels, extent=extent, asRaster=TRUE, corners=TRUE)
+	z <- getValues(r) + 1
+
+	if (! is.null(coltab) ) {
+		z <- matrix(coltab[z], nrow=nrow(r), ncol=ncol(r), byrow=T)
+		z <- as.raster(z)
+	} else {
+		z <- matrix(z, nrow=nrow(r), ncol=ncol(r), byrow=T)
+		z <- as.raster(z, max=max(z, na.rm=TRUE))
+	}
+
+	require(grDevices)
+	plot(c(xmin(r), xmax(r)), c(ymin(r), ymax(r)), type = "n", xlab="", ylab="")
+	rasterImage(z, xmin(r), ymin(r), xmax(r),  ymax(r), interpolate=interpolate)
+}
+)
+
