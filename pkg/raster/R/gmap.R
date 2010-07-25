c967940c4d85c4e6425eede238dcfa71e51c68fc
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-07-25 07:34:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1045 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/gmap.R
+++ pkg/raster/R/gmap.R
@@ -3,13 +3,17 @@
 # Version 1.0
 # Licence GPL v3
 
-# Based on functions in R pacakge 'RgoogleMaps' 
+# Based on functions in R package 'RgoogleMaps' 
 # by Markus Loecher, Sense Networks <markus at sensenetworks.com>
 
 
-gmap <- function (x , exp=1, maptype='terrain', filename='', key, ...) {
+gmap <- function (x, exp=1, type='terrain', filename='', key, ...) {
 
-    size = c(640, 640)
+	if (! .requireRgdal() ) { stop('rgdal not available') }
+	
+	if (! type %in% c('roadmap', 'satellite', 'hybrid', 'terrain')) {
+		warning("type should be: roadmap, satellite, hybrid, or terrain.") 
+	}
 
 	mxzoom <- function (latrange, lonrange, size = c(640, 640)) {
 		SinPhi = sin(latrange * pi/180)
@@ -58,10 +62,16 @@ gmap <- function (x , exp=1, maptype='terrain', filename='', key, ...) {
 		return(list(X = X, Y = Y))
 	}
 
-	if (inherits(x, 'Raster')) {
-		# check if long/lat, if not project
-	}
-	x <- extent(x)
+	x <- raster(x)
+	if ( .couldBeLonLat(x, FALSE) ) {
+		x <- extent(x)
+	} else {
+		if ( projection(x) == "NA" ) {
+			stop('CRS is unknown, and does not look like Lon/Lat')
+		} else {
+			x <- extent( projectExtent(x, "+proj=longlat +datum=WGS84") )
+		}
+	} 
 	
 	e <- x * exp
 	e@xmin <- max(-180, e@xmin)
@@ -72,10 +82,10 @@ gmap <- function (x , exp=1, maptype='terrain', filename='', key, ...) {
     lonR <- c(e@xmin, e@xmax)
 	latR <- c(e@ymin, e@ymax)
 
+    size <- c(640, 640)
     zoom <- min(mxzoom(latR, lonR, size))
-	center = c(mean(latR), mean(lonR))
+	center <- c(mean(latR), mean(lonR))
  	
-	
     ll <- ll2XY(latR[1], lonR[1], zoom)
     ur <- ll2XY(latR[2], lonR[2], zoom)
     cr <- ll2XY(center[1], center[2], zoom)
@@ -90,7 +100,6 @@ gmap <- function (x , exp=1, maptype='terrain', filename='', key, ...) {
         s <- paste(size, collapse = "x")
     }
 
-
 	if (missing(key)) {
 		KeyFile <- paste(Sys.getenv("HOME"), "/API.key.txt", sep = "")
 		if (file.exists(KeyFile)) {
@@ -100,34 +109,34 @@ gmap <- function (x , exp=1, maptype='terrain', filename='', key, ...) {
 		}
 	}
 
-
 	if (trim(filename) == '') filename <- rasterTmpFile()
 	ext(filename) <- 'gif'
 	ctr <- paste(center, collapse = ",")
 	
 	gurl <- "http://maps.google.com/staticmap?"
-    gurl <- paste(gurl, "center=", ctr, "&zoom=", zoom, "&size=", s, "&maptype=", maptype, "&format=gif", "&key=", key, "&sensor=false", sep = "")
+    gurl <- paste(gurl, "center=", ctr, "&zoom=", zoom, "&size=", s, "&maptype=", type, "&format=gif", "&key=", key, "&sensor=false", sep = "")
 	download.file(gurl, filename, mode = "wb", quiet = TRUE)
-   
+    
 	MyMap <- list(lat.center = center[1], lon.center = center[2], zoom = zoom)
 	bb <- list(ll = xy2ll(MyMap, X = -size[1]/2 + 0.5, Y = -size[2]/2 - 0.5), ur = xy2ll(MyMap, X = size[1]/2 +  0.5, Y = size[2]/2 - 0.5))
 
 	r <- raster(filename)
-	
 	ext <- extent(bb$ll[2], bb$ur[2], bb$ll[1], bb$ur[1])
 	p <- t(bbox(raster(ext))) *  pi/180
 	rad = 6378137	
-    y <- log(tan(p[, 2]) + (1/cos(p[, 2]))) * rad
-    x <- p[, 1] * rad
-    xy <- cbind(x, y)
-	extent(r) <- extent(as.vector(xy))
+    p[,2] <- log(tan(p[, 2]) + (1/cos(p[, 2]))) 
+    p <- p * rad
+	extent(r) <- extent(as.vector(p))
 	projection(r) = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
+	worldFile(r, extension = ".gfw") 
+	ext(filename) <- 'prj'
+	showWKT(projection(r), filename, morphToESRI=TRUE)
     return(r)
 }
 
-#e = c( -121.9531 , -120.3897 , 35.36 , 36.61956 )
+#e = extent( -121.9531 , -120.3897 , 35.36 , 36.61956 )
 #r = gmap(e)
-#plotCT(r)
+#plot(r)
 
 #projmerc <- function(p) {
 #    p <- p * pi / 180
