656d4a3196db8429472b112d8af89c1d55261328
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-08-02 17:52:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1060 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/clump2.R
+++ pkg/raster/R/clump2.R
@@ -17,20 +17,22 @@ if (!isGeneric("clump")) {
 		return( setValues(x1, NA) )
 	}
 	adjv <- as.vector( t ( adjacency(x1, val, val, directions=directions) ) )
-	adjv <- c(adjv, rep(val, each=2))  # fixed problem of missing the last cells, perhaps clumsy
+	
+	add = val[! val %in% adjv]		   # RH
+	adjv <- c(adjv, rep(add, each=2))  # fixed problem of missing the last single cells, perhaps clumsy?
 	
 	cl <- clusters(graph(adjv, directed=FALSE))$membership[val+1]
 	ucl <- sort(unique(cl))
 	ucl <- data.frame(id=ucl, value=1:length(ucl))
 
-	x1[val] <- as.numeric(as.factor(cl)) # force 1 to n
+	x1[val] <- as.numeric(as.factor(cl)) # RH force 1 to n
 	return(x1)
 }
 
 
 setMethod('clump', signature(x='RasterLayer'), 
 
-function(x, filename='', directions=8, gaps=TRUE, ...) {
+function(x, filename='', directions=8, gaps=TRUE, datatype='INT2U', ...) {
 
 	if( !require(igraph)) {
 		stop('you need to install the igraph package to be able to use this function')
@@ -66,47 +68,20 @@ function(x, filename='', directions=8, gaps=TRUE, ...) {
 	maxval <- 0
 	
 	rcl <- matrix(nrow=0, ncol=2)
+	
 	for (i in 1:tr$n) {
 	
 		ext[4] <- yFromRow(outRaster, tr$row[i]) + 0.5 * yres(outRaster)
 		
 		endrow <- tr$row[i] + tr$nrows[i] - 1 
-		ext[3] <- yFromRow(outRaster, endrow) - 1.5 * yres(outRaster) # one additional row
+		ext[3] <- yFromRow(outRaster, endrow) - 1.5 * yres(outRaster) # one additional row for overlap
 		xc <- crop(x, extent(ext))
 		
 		xc <- .smallClump(xc, directions) + maxval
 		if (i > 1) {
 			firstrow <- getValues(xc, 1)
 			rc <- na.omit(unique(cbind(lastrow, firstrow)))
-			if (nrow(rc) > 1) {
-				# here we need to catch the case where one number becomes two other numbers
-				tt <- table(rc[,1])
-				j <- as.integer(names(tt[ which(tt > 1) ]))
-				if ( length(j) > 0 ) {
-					k <- rc[,1] %in% j
-					rc2 <- rc[!k,]
-					rcl <- rbind(rcl, rc2)
-					ss <- rc[k,]
-					h <- cbind(ss[,2], ss[,1])
-					tt2 <- table(h[,1])
-					j2 <- as.integer(names(tt2[ which(tt2 > 1) ]))
-					if (length(j2) > 0) {
-						k2 <- h[,1] %in% j2
-						hh <- sort(unique(h[k2,2]))
-						if (length(hh) > 1) {
-							hh <- cbind(hh[-1], hh[1])
-							rcl <- rbind(rcl, hh)
-						}
-						h[h[, 2] %in% h[h[,1] == j2,2], 2] <- min(h[h[,1] == j2,2])
-					}
-					h <- unique(h)
-					xc <- subs(xc, data.frame(h), subsWithNA=FALSE)
-					rcl <- rbind(rcl, h)
-					
-				} else {
-					rcl <- rbind(rcl, rc)
-				}
-			}
+			rcl <- rbind(rcl, rc)
 		}
 		lastrow <- getValues(xc, nrow(xc))
 		
@@ -117,29 +92,24 @@ function(x, filename='', directions=8, gaps=TRUE, ...) {
 	outRaster <- writeStop(outRaster)
 	pbClose(pb)
 	
-	# now reclass
-	if (nrow(rcl) > 1) {
-		i = rcl[rcl[,2] %in% rcl[,1],2]
-		s = rcl[rcl[,1] %in% i,]
-		if (nrow(s) > 0) {
-			for (b in 1:nrow(s)) { 
-				rcl[rcl[,2]==s[b,1],2] <- s[b,2] 
-			}
-		}
 	
-		if (gaps) {
-			outRaster <- subs(outRaster, data.frame(rcl), subsWithNA=FALSE, filename=filename, ...)
-			return(outRaster)
-		} else {
-			outRaster <- subs(outRaster, data.frame(rcl), subsWithNA=FALSE, filename='', datatype='INT2U')
-			un <- unique(outRaster)
-			un <- data.frame(cbind(un, 1:length(un)))
-			outRaster <- subs(outRaster, un, filename=filename, ...)
-		}
-	}
-	if (filename == '') {
-		filename <- rasterTmpFile()
+	if (nrow(rcl) > 0) {
+		g <- graph.edgelist(rcl, directed=FALSE)
+		cl <- clusters(g)$membership
+		rc <- cbind(V(g),cl)
+		i <- rc[,1] != rc[,2]
+		rc <- rc[i, ,drop=FALSE]
+		outRaster <- subs(outRaster, data.frame(rc), subsWithNA=FALSE, filename=filename, datatype=datatype, ...)
+		return(outRaster)
+		
+	} else if (!gaps) {
+		un <- unique(outRaster)
+		un <- data.frame(cbind(un, 1:length(un)))
+		outRaster <- subs(outRaster, un, subsWithNA=FALSE, filename=filename, datatype=datatype, ...)
+		
+	} else if (filename != '') {
 		return( writeRaster(outRaster, filename=filename, ...) )
+		
 	} else {
 		return(outRaster)
 	}
