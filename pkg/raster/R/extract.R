3ad2ecdd385cbbf5f4ec0610965addb033e8c451
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-10-20 00:44:42 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1238 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/raster/R/extract.R
@@ -0,0 +1,55 @@
+# Author: Robert J. Hijmans, r.hijmans@gmail.com
+# Date : October 2010
+# Version 1.0
+# Licence GPL v3
+
+
+if (!isGeneric("extract")) {
+	setGeneric("extract", function(x, y, ...)
+		standardGeneric("extract"))
+}	
+
+
+setMethod('extract', signature(x='Raster', y='vector'), 
+function(x, y, ...){ 
+	return(cellValues(x, y, ...))
+})
+
+
+setMethod('extract', signature(x='Raster', y='matrix'), 
+function(x, y, ...){ 
+	return(xyValues(x, y, ...))
+})
+
+
+setMethod('extract', signature(x='Raster', y='data.frame'), 
+function(x, y, ...){ 
+	return(xyValues(x, y, ...))
+})
+
+
+setMethod('extract', signature(x='Raster', y='SpatialPoints'), 
+function(x, y, ...){ 
+	return(xyValues(x, y, ...))
+})
+
+
+setMethod('extract', signature(x='Raster', y='SpatialLines'), 
+function(x, y, ...){ 
+	lineValues(y, x, ...)
+})
+
+
+
+setMethod('extract', signature(x='Raster', y='SpatialPolygons'), 
+function(x, y, ...){ 
+	polygonValues(y, x, ...)
+})
+
+
+
+setMethod('extract', signature(x='Raster', y='Extent'), 
+function(x, y, ...){ 
+	return( getValues(crop(x, y, ...)) )
+})
+
