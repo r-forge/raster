89d967965fe6bbcb5d9429240b71d143de8a6635
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-01-14 01:53:29 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1487 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/indexReplace.R
+++ pkg/raster/R/indexReplace.R
@@ -4,7 +4,69 @@
 # Licence GPL v3
 
 
-setReplaceMethod("[", "RasterLayer",
+
+setReplaceMethod("[", c("RasterLayer", "RasterLayer"),
+	function(x, i, j, value) {
+
+		if (! missing(j) ) { 
+			stop('if the first index is a RasterLayer object, there cannot be a second index') 
+		}	
+	
+		if (! hasValues(i) ) {
+			i <- cellsFromExtent(x, i)
+			
+		} else if (compare(x, i, stopiffalse=FALSE, showwarning=FALSE)) {
+			i <- as.logical( getValues(i) )
+			i[is.na(i)] <- FALSE
+			
+		} else {
+			i <- cellsFromExtent(x, i)
+		}		
+	
+		return(callNextMethod(x, i, j, value=value))
+	}
+)
+
+
+setReplaceMethod("[", c("RasterLayer", "Extent"),
+	function(x, i, j, value) {
+
+		if (! missing(j) ) { 
+			stop('if the first index is an Extent oject, there cannot be a second index') 
+		}	
+		
+		i <- cellsFromExtent(x, i)
+		return(callNextMethod(x, i, j, value=value))
+	}
+)
+
+setReplaceMethod("[", c("RasterLayer", "Spatial"),
+	function(x, i, j, value) {
+
+		if (! missing(j) ) { 
+			stop('if the first index is a Spatial* object, there cannot be a second index') 
+		}	
+	
+		if (inherits(i, 'SpatialPolygons')) {
+			v <- 1:length(i@polygons)
+			v[] <- value
+			return( .polygonsToRaster(i, x, field=v, fun='last', mask=FALSE, update=TRUE, updateValue="all", silent=TRUE) )
+			
+		} else if (inherits(i, 'SpatialLines')) {
+			v <- 1:length(i@lines)
+			v[] <- value
+			return( .linesToRaster(i, x, field=v, fun='last', mask=FALSE, update=TRUE, updateValue="all", silent=TRUE) )
+			
+		} else { # if (inherits(i, 'SpatialPoints')) {
+			i <- cellsFromXY(x, coordinates(i))
+			return( callNextMethod(x, i, j, value=value) )
+		}
+	}
+)
+
+
+
+setReplaceMethod("[", c("RasterLayer","ANY"),
 	function(x, i, j, value) {
 
 	if (! missing(j) ) { 
@@ -12,16 +74,21 @@ setReplaceMethod("[", "RasterLayer",
 			stop('the second argument must be numeric (or missing)') 
 		}	
 		if (! missing(i)) {
-			if (! (is.numeric(i) | is.logical(i)) ) {
-				stop('you cannot supply a second argument if the first is not numeric or logical') 		
+			if (! is.numeric(i)) {
+				stop('the first index must be numeric if you supply a second index') 
 			}
 		}
-	}
+	} 
 
-	if (! is.numeric(value) & !is.logical(value)) { value <- as.numeric(value) }
+	if (! is.numeric(value) & !is.logical(value)) { 
+		value <- as.numeric(value) 
+	}
 		
 	if ( missing(i) ) {
+	
 		if (missing(j)) {
+			# all cells
+			
 			if (length(value) == ncell(x)) {
 				x <- try( setValues(x, value))
 			} else if (length(value) == 1) {
@@ -37,32 +104,19 @@ setReplaceMethod("[", "RasterLayer",
 				stop('cannot replace values on this raster (it is too large')
 			}
 			return(x)
+			
 		} else {
+			# columns
 			i <- cellFromCol(x, j)
 		} 
+		
 	} else {
-		if (inherits(i, 'Spatial')) {
-			if (inherits(i, 'SpatialPolygons')) {
-				v <- 1:length(i@polygons)
-				v[] <- value
-				return( .polygonsToRaster(i, x, field=v, fun='last', mask=FALSE, update=TRUE, updateValue="all", silent=TRUE) )
-			}
-			if (inherits(i, 'SpatialLines')) {
-				v <- 1:length(i@lines)
-				v[] <- value
-				return( .linesToRaster(i, x, field=v, fun='last', mask=FALSE, update=TRUE, updateValue="all", silent=TRUE) )
-			}
-			stop('currently only implemented for SpatialLines* and SpatialPolygons*, not for other Spatial* objects')
-		} else if (inherits(i, "RasterLayer")) {
-			if (compare(x, i, stopiffalse=FALSE, showwarning=FALSE)) {
-				i <- as.logical( getValues(i) )
-				i[is.na(i)] <- FALSE
-			} else {
-				i <- extent(i)
-			}
-		} else if (inherits(i, "Extent")) {
-			i <- cellsFromExtent(x, i)
-		} else if (missing(j)) {
+
+		if (! (is.numeric(i) | is.logical(i)) ) {
+			stop('the first index must be numeric or logical') 
+		}
+	
+		if (missing(j)) {
 			theCall <- sys.call(-1)
 			narg <- length(theCall)-length(match.call(call=sys.call(-1)))
 			if (narg > 0) {
@@ -73,8 +127,10 @@ setReplaceMethod("[", "RasterLayer",
 		}
 	}
 
-	i <- na.omit(i)
-	if (! is.logical(i) ) {
+	if ( is.logical(i) ) {
+		i[is.na(i)] <- FALSE
+	} else {
+		i <- na.omit(i)
 		i <- subset(i, i >= 1 & i <= ncell(x))
 	}
 	
