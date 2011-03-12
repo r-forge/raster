0e452bf023693948578201740f7f60fbfe838fed
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-03-12 01:02:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1563 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/raster/R/slope.R
@@ -0,0 +1,105 @@
+# Author: Robert J. Hijmans
+# Date : February 2010
+# Version 1.0
+# Licence GPL v3
+
+
+slopeAspect <- function(alt, filename='', type='', unit='', rotateAspect=FALSE, ...) {
+	
+	type <- tolower(type)
+	stopifnot(type %in% c('', 'slope', 'aspect'))
+	unit <- tolower(unit)
+	stopifnot(unit %in% c('degrees', ''))
+	filename <- trim(filename)
+	
+	res <- res(alt)
+	xres <- res[1]
+	yres <- res[2]
+	fX <- matrix(c(-1,-2,-1,0,0,0,1,2,1), nrow=3) * -1
+	fY <- matrix(c(-1,0,1,-2,0,2,-1,0,1), nrow=3) 
+	
+	if (raster:::.couldBeLonLat(alt)) {
+		yres <- pointDistance(cbind(0,0), cbind(0,yres), longlat=TRUE)
+		fY <- fY / yres
+		zy <- focalFilter(alt, fY)
+		zx <- focalFilter(alt, fX)
+		
+		y <- yFromRow(alt, 1:nrow(alt))
+		dx <- raster:::.haversine(-xres, y, xres, y) / 3
+		zx <- t( t(zx) / dx)
+		
+	} else {
+	
+		fX <- fX / xres
+		fY <- fY / yres
+		zx <- focalFilter(alt, fX)
+		zy <- focalFilter(alt, fY)
+	}
+
+	if (type == 'slope') {
+		
+		x <- sqrt( zy^2 + zx^2 ) 
+		if (unit == 'degrees') {
+			x = atan(x) / (pi / 180)
+		}
+		layerNames(x) <- 'slope'
+		
+	} else if (type == 'aspect') {
+		x <- atan2(zy, zx)
+		if (rotateAspect) {
+			x = x %%(2*pi) 
+		}
+		if (unit == 'degrees') {
+			x <- x / (pi / 180)
+		}
+		layerNames(x) <- 'aspect'
+		
+	} else {
+		slope <- sqrt( zy^2 + zx^2 ) 
+		aspect <- atan2(zy, zx) 
+		if (rotateAspect) {
+			aspect = aspect %%(2*pi) 
+		}
+		
+		if (unit == 'degrees') {
+			slope <- atan(slope) / (pi / 180)
+			aspect <- aspect / (pi / 180)
+		}
+		
+		layerNames(slope) <- 'slope'
+		layerNames(aspect) <- 'aspect'
+		x <- stack(slope, aspect)
+	}
+
+	if (filename != "") {
+		x <- writeRaster(x, filename, ...)
+	}
+	return(x)
+}
+
+
+hillShade1 <- function(alt, zenith, azimuth, filename='', ...) {
+	zenith <- zenith * pi/180
+	azimuth <- azimuth * pi/180
+	slopeasp <- slopeAspect(alt, filename='', type='both', unit='', rotateAspect=TRUE) 
+	x <- cos(slopeasp[[1]]) * cos(zenith) + sin(slopeasp[[1]]) * sin(zenith) * cos(azimuth-slopeasp[[2]])
+	filename <- trim(filename)
+	if (filename != "") {
+		x <- writeRaster(x, filename, ...)
+	}
+	return(x)
+#	plot(x, col=gray(1:100/100))
+}
+
+
+hillShade2 <- function(slope, aspect, zenith, azimuth, filename='', ...) {
+	zenith = zenith * pi/180
+	azimuth = azimuth * pi/180
+	x <- cos(slope) * cos(zenith) + sin(slope) * sin(zenith) * cos(azimuth-aspect)
+	if (filename != "") {
+		x <- writeRaster(x, filename, ...)
+	}
+	return(x)
+#	plot(x, col=gray(1:100/100))
+}
+
