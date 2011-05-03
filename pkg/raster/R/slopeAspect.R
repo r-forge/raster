b207cea271883349a1c02b75ed6e594e7ac87640
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-05-03 19:18:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1654 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/slopeAspect.R
+++ pkg/raster/R/slopeAspect.R
@@ -4,14 +4,24 @@
 # Licence GPL v3
 
 
-slopeAspect <- function(dem, filename='', type='both', unit='radians', neighbors=8, flatAspect, ...) {
-	type <- trim(tolower(type))
-	stopifnot(type %in% c('', 'both' , 'slope', 'aspect'))
+slopeAspect <- function(dem, filename='', out=c('slope', 'aspect'), unit='radians', neighbors=8, flatAspect, ...) {
+
+	stopifnot(neighbors %in% c(4, 8))
+	stopifnot(projection(dem) != "NA")
 	unit <- trim(tolower(unit))
 	stopifnot(unit %in% c('degrees', 'radians'))
 	filename <- trim(filename)
-	stopifnot(neighbors %in% c(4, 8))
-	stopifnot(projection(dem) != "NA")
+
+	out <- trim(tolower(out))
+	stopifnot(all(out %in% c('slope', 'aspect')))
+	
+	
+	if (length(out) == 1) {
+		type <- out
+	} else {
+		type <- 'both'
+	}
+	
 	
 	res <- res(dem)
 	dx <- res[1]
