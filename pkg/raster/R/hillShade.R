9596b06c86bbef63b5b869642cfb498c57317d9a
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-05-03 05:46:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1653 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/hillShade.R
+++ pkg/raster/R/hillShade.R
@@ -1,16 +1,16 @@
-# Author: Andrew Bevan and Robert J. Hijmans
+# Author: Andrew Bevan, Oscar Perpiñán Lamigueiro, and Robert J. Hijmans
 # Date : March 2010
 # Version 1.0
 # Licence GPL v3
 
-
-hillShade <- function(slope, aspect, declination, direction, filename='', ...) {
+hillShade <- function(slope, aspect, angle=45, direction=0, filename='', ...) {
 	compare(slope, aspect)
-	declination <- declination * pi/180
-	direction <- direction * pi/180
 
+	direction <- direction * pi/180
+	zenith <- (90 - angle)*pi/180
+	
 	#x <- cos(slope) * cos(declination) + sin(slope) * sin(declination) * cos(direction-aspect)
-	fun <- function(slp, asp) { cos(slp) * cos(declination) + sin(slp) * sin(declination) * cos(direction-asp) }
+	fun <- function(slp, asp) { cos(slp) * cos(zenith) + sin(slp) * sin(zenith) * cos(direction-asp) }
 	x <- overlay(slope, aspect, fun=fun, filename=filename, ...)		
 	return(x)
 }
