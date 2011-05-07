a1b90fdee040111c09cd5348a176d42b9071d073
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-05-07 04:11:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1659 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/stackSelect2.R
+++ pkg/raster/R/stackSelect2.R
@@ -1,81 +1,115 @@
 # Author: Robert J. Hijmans, r.hijmans@gmail.com
-# Date:  March 2011
+# Date:  May 2011
 # Version 1
 # Licence GPL v3
 
 
 setMethod('stackSelect', signature(x='RasterStackBrick', y='RasterStackBrick'), 
-function(x, y, fun=sum, filename='', na.rm=FALSE, ...) {
+function(x, y, recycle=FALSE, filename='', na.rm=FALSE, ...) {
 
 	nlx <- nlayers(x)
 	nly <- nlayers(y)
-	stopifnot(nlx > nly)
-	stopifnot(nlx %% nly == 0)
 	filename <- trim(filename)
 
-	nrounds <- nlx / nly
-	if (nrounds == 1) {
-		out <- raster(x)
-	} else {
-		out <- brick(x)	
-		out@data@nlayers <- as.integer(nrounds)
-	}
-	compare(out, y)
-	
-	
-	if (canProcessInMemory(x, nlx+nly+(2*nrounds))) {
-	
-		i <- round(getValues(y))
-		i[i < 1 | i > nly] <- NA
-		m <- matrix(nr=ncell(out), nc=nrounds)
-		n <- matrix(nr=ncell(out), nc=nly)
+	if (recursive) {
+		stopifnot(nly > 1)
+		stopifnot(nlx > nly)
+		stopifnot(nlx %% nly == 0)
 		
-		x <- as.matrix(getValues(x))
-		for (r in 1:nrounds) {
-			sc <- ((r-1)*nly+1)
-			xx <- x[, sc:(sc+nly-1), drop=FALSE]
-			a <- cbind(1:nrow(xx), i)
-			for (p in 1:nly) {
-				n[,p] <- xx[a[,c(1, (1+p))]]
+		nrounds <- nlx / nly
+		out <- brick(x, values=FALSE)	
+		compare(out, y)
+	
+		ib <- (nlx+1):(nlx+nly)
+	
+		if (canProcessInMemory(x, nlx+nlx+nly)) {
+	
+			y <- round(getValues(y))
+			y[y < 1 | y > nly] <- NA
+			m <- matrix(nr=ncell(out), nc=nlx)
+			x <- cbind(getValues(x), y)
+			for (r in 1:nrounds) {
+				ia <- ((r-1)*nly+1)
+				ia <- ia:(ia+nly)
+				n <- apply(x, 1, function(x) x[ia][ x[ib]] )
+				m[,ia] <- t(n)
 			}
-			m[,r] <- apply(n, 1, fun, na.rm=na.rm)
+			out <- setValues(out, m)
+			if (filename != "") {
+				out <- writeRaster(out, filename=filename, ...)
+			}
+			return(out)
 		}
-		out <- setValues(out, m)
-		if (filename != "") {
-			out <- writeRaster(out, filename=filename, ...)
+	
+		if (filename == '') { filename <- rasterTmpFile() } 
+	
+		out <- writeStart(out, filename=filename, ...)
+		tr <- blockSize(out, n=nlx+nly)
+		pb <- pbCreate(tr$n, type=.progress(...))
+
+		for (i in 1:tr$n) {
+			j <- round(getValues(y, row=tr$row[i], nrows=tr$nrows[i]))
+			j[j < 1 | j > nly] <- NA
+			v <- getValues(x, row=tr$row[i], nrows=tr$nrows[i])
+			v <- cbind(v, j)
+			m <- matrix(nr=nrow(v), nc=nrounds)
+			for (r in 1:nrounds) {
+				ia <- ((r-1)*nly+1)
+				ia <- ia:(ia+nly)
+				n <- apply(x, 1, function(x) x[ia][ x[ib]] )
+				m[,ia] <- t(n)
+			}	
+			out <- writeValues(out, m, tr$row[i])
+			pbStep(pb) 
 		}
+		out <- writeStop(out)
+		pbClose(pb)
+		return(out)
+		
+	} else {
+	
+		if (nly == 1) {
+			out <- raster(x)
+		} else {
+			out <- brick(x, values=FALSE)	
+			out@data@nlayers <- nlayers(y)
+		}
+		compare(out, y)
+	
+		ib <- (nlx+1):(nlx+nly)
+	
+		if (canProcessInMemory(x, nlx+nly+(2*nrounds))) {
+		
+			y <- round(getValues(y))
+			y[y < 1 | y > nly] <- NA
+			x <- cbind(getValues(x), y)
+			m <- apply(x, 1, function(x)x[1:nlx][x[ib]] )
+			out <- setValues(out, m)
+			if (filename != "") {
+				out <- writeRaster(out, filename=filename, ...)
+			}
+			return(out)
+		}
+	
+		if (filename == '') { filename <- rasterTmpFile() } 
+	
+		out <- writeStart(out, filename=filename, ...)
+		tr <- blockSize(out, n=nlx+nly)
+		pb <- pbCreate(tr$n, type=.progress(...))
+
+		for (i in 1:tr$n) {
+			j <- round(getValues(y, row=tr$row[i], nrows=tr$nrows[i]))
+			j[j < 1 | j > nly] <- NA
+			v <- getValues(x, row=tr$row[i], nrows=tr$nrows[i])
+			v <- cbind(v, j)
+			m <- apply(x, 1, function(x)x[1:nlx][x[ib]] )
+			out <- writeValues(out, m, tr$row[i])
+			pbStep(pb) 
+		}
+		out <- writeStop(out)
+		pbClose(pb)
 		return(out)
 	}
-	
-	if (filename == '') { filename <- rasterTmpFile() } 
-	
-	out <- writeStart(out, filename=filename, ...)
-	tr <- blockSize(out, n=nlx+nly)
-	pb <- pbCreate(tr$n, type=.progress(...))
-
-	for (i in 1:tr$n) {
-		j <- round(getValues(y, row=tr$row[i], nrows=tr$nrows[i]))
-		j[j < 1 | j > nly] <- NA
-		v <- as.matrix(getValues(x, row=tr$row[i], nrows=tr$nrows[i]))
-		
-		n <- matrix(nr=nrow(v), nc=nly)
-		m <- matrix(nr=nrow(v), nc=nrounds)
-		for (r in 1:nrounds) {
-			sc <- ((r-1)*nly+1)
-			xx <- v[, sc:(sc+nly-1), drop=FALSE]
-			a <- cbind(1:nrow(xx), j)
-			for (p in 1:nly) {
-				n[,p] <- xx[a[,c(1, (1+p))]]
-			}
-			m[,r] <- apply(n, 1, fun, na.rm=na.rm)			
-		}
-		out <- writeValues(out, m, tr$row[i])
-		pbStep(pb) 
-	}
-
-	out <- writeStop(out)
-	pbClose(pb)
-	return(out)
 } )
 	
 
