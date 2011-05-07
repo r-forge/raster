1016e2fb9b1f7c121bd8a0ee2bc1def8e032630f
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-05-07 05:52:52 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1660 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/stackSelect2.R
+++ pkg/raster/R/stackSelect2.R
@@ -1,4 +1,4 @@
-# Author: Robert J. Hijmans, r.hijmans@gmail.com
+# Author: Robert J. Hijmans
 # Date:  May 2011
 # Version 1
 # Licence GPL v3
@@ -10,88 +10,43 @@ function(x, y, recycle=FALSE, filename='', na.rm=FALSE, ...) {
 	nlx <- nlayers(x)
 	nly <- nlayers(y)
 	filename <- trim(filename)
+	out <- brick(x, values=FALSE)	
+	compare(out, y)
 
-	if (recursive) {
+	if (recycle) {
 		stopifnot(nly > 1)
 		stopifnot(nlx > nly)
 		stopifnot(nlx %% nly == 0)
+		nl <- nlx+nlx+nly
+		nr <- nlx / nly
+		id <- as.integer( (rep(1:nr, each=nly)-1) * nly )
 		
-		nrounds <- nlx / nly
-		out <- brick(x, values=FALSE)	
-		compare(out, y)
-	
-		ib <- (nlx+1):(nlx+nly)
-	
-		if (canProcessInMemory(x, nlx+nlx+nly)) {
-	
-			y <- round(getValues(y))
-			y[y < 1 | y > nly] <- NA
-			m <- matrix(nr=ncell(out), nc=nlx)
-			x <- cbind(getValues(x), y)
-			for (r in 1:nrounds) {
-				ia <- ((r-1)*nly+1)
-				ia <- ia:(ia+nly)
-				n <- apply(x, 1, function(x) x[ia][ x[ib]] )
-				m[,ia] <- t(n)
-			}
-			out <- setValues(out, m)
-			if (filename != "") {
-				out <- writeRaster(out, filename=filename, ...)
-			}
-			return(out)
+	} else {
+		if (nly == 1) {
+			out <- raster(out)
+		} else {
+			out@data@nlayers <- nlayers(y)
 		}
-	
-		if (filename == '') { filename <- rasterTmpFile() } 
-	
-		out <- writeStart(out, filename=filename, ...)
-		tr <- blockSize(out, n=nlx+nly)
-		pb <- pbCreate(tr$n, type=.progress(...))
+		nl <- nlx+nly
+		id <- 0
+	}
 
-		for (i in 1:tr$n) {
-			j <- round(getValues(y, row=tr$row[i], nrows=tr$nrows[i]))
-			j[j < 1 | j > nly] <- NA
-			v <- getValues(x, row=tr$row[i], nrows=tr$nrows[i])
-			v <- cbind(v, j)
-			m <- matrix(nr=nrow(v), nc=nrounds)
-			for (r in 1:nrounds) {
-				ia <- ((r-1)*nly+1)
-				ia <- ia:(ia+nly)
-				n <- apply(x, 1, function(x) x[ia][ x[ib]] )
-				m[,ia] <- t(n)
-			}	
-			out <- writeValues(out, m, tr$row[i])
-			pbStep(pb) 
+	ib <- (nlx+1):(nlx+nly)
+
+	if (canProcessInMemory(x, nl)) {
+	
+		y <- round(getValues(y))
+		y[y < 1 | y > nly] <- NA
+		x <- cbind(getValues(x), y)
+		n <- apply(x, 1, function(z) z[z[ib]+id] )
+		out <- setValues(out, t(n))
+		if (filename != "") {
+			out <- writeRaster(out, filename=filename, ...)
 		}
-		out <- writeStop(out)
-		pbClose(pb)
-		return(out)
 		
 	} else {
 	
-		if (nly == 1) {
-			out <- raster(x)
-		} else {
-			out <- brick(x, values=FALSE)	
-			out@data@nlayers <- nlayers(y)
-		}
-		compare(out, y)
-	
-		ib <- (nlx+1):(nlx+nly)
-	
-		if (canProcessInMemory(x, nlx+nly+(2*nrounds))) {
-		
-			y <- round(getValues(y))
-			y[y < 1 | y > nly] <- NA
-			x <- cbind(getValues(x), y)
-			m <- apply(x, 1, function(x)x[1:nlx][x[ib]] )
-			out <- setValues(out, m)
-			if (filename != "") {
-				out <- writeRaster(out, filename=filename, ...)
-			}
-			return(out)
-		}
-	
-		if (filename == '') { filename <- rasterTmpFile() } 
+		if (filename == '') { filename <- rasterTmpFile() }
 	
 		out <- writeStart(out, filename=filename, ...)
 		tr <- blockSize(out, n=nlx+nly)
@@ -102,14 +57,13 @@ function(x, y, recycle=FALSE, filename='', na.rm=FALSE, ...) {
 			j[j < 1 | j > nly] <- NA
 			v <- getValues(x, row=tr$row[i], nrows=tr$nrows[i])
 			v <- cbind(v, j)
-			m <- apply(x, 1, function(x)x[1:nlx][x[ib]] )
-			out <- writeValues(out, m, tr$row[i])
+			n <- apply(v, 1, function(z) z[z[ib]+id] )
+			out <- writeValues(out, t(n), tr$row[i])
 			pbStep(pb) 
 		}
 		out <- writeStop(out)
 		pbClose(pb)
-		return(out)
 	}
+	return(out)
 } )
-	
-
+		
