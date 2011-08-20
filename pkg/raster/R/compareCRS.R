e959806a2dd85e248ba567c02d67fc2aba2401f6
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-08-20 18:13:29 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1826 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/compareCRS.R
+++ pkg/raster/R/compareCRS.R
@@ -5,21 +5,35 @@
 
 
 .compareCRS <- function(x, y) {
-	if (class(x) == 'CRS') { x <- x@projargs }
-	if (class(y) == 'CRS') { y <- y@projargs }
-	if (inherits(x, 'Raster')) { x <- projection(x) }
-	if (inherits(x, 'Raster')) { y <- projection(y) }
-	x <- trim(x)
-	y <- trim(y)
-	x = unlist( strsplit(x, ' ') )
-	y = unlist( strsplit(y, ' ') )
-	xl <- length(x)
-	yl <- length(y)
-	
-	if (x[[1]] != y[[1]]) { return(FALSE) }
-
-	# etc...
-	
+	if (class(x) == 'character') { 
+	} else if (class(x) == 'CRS') { 
+		x <- x@projargs 
+	} else {
+		x <- projection(x) 
+	}
+	if (class(y) == 'character') { 
+	} else if (class(y) == 'CRS') { 
+		y <- y@projargs 
+	} else {
+		y <- projection(y)
+	}
+	x <- gsub(' ', '', x)
+	y <- gsub(' ', '', y)
+	x <- unlist( strsplit(x, '+', fixed=TRUE) )[-1]
+	y <- unlist( strsplit(y, '+', fixed=TRUE) )[-1]
+	a <- strsplit(x, '=')
+	a <- do.call(rbind, a)
+	b <- strsplit(y, '=')
+	b <- do.call(rbind, b)
+	i <- which(a[,1] %in% b[,1])
+	j <- which(b[,1] %in% a[,1])
+	a <- a[i,]
+	b <- b[j,]
+	a <- a[order(a[,1]),]
+	b <- b[order(b[,1]),]
+	if (! all(a[,2] == b[,2])) {
+		return(FALSE)
+	}
 	return(TRUE)
 }
 
