fe177cdc2644627f5fd71ec38fc2de97e95fb783
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-09-19 07:21:49 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1858 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/adjacent.R
+++ pkg/raster/R/adjacent.R
@@ -5,20 +5,31 @@
 
 
 adjacent <- function(x, cells, directions=4) {
+	pairs=FALSE
 	stopifnot(directions %in% c(4,8))
 	r <- res(x)
 	xy <- xyFromCell(x, cells)
 
 	if (directions==4) {
-		d <- t(apply(xy, 1, function(x)c(x[1]-r[1], x[1]+r[1], x[1], x[1], x[2], x[2], x[2]+r[2], x[2]-r[2])))
+		fun <- function(x)c(x[1]-r[1], x[1]+r[1], x[1], x[1], x[2], x[2], x[2]+r[2], x[2]-r[2])
 	} else { #if (directions==8) {
-		d <- t(apply(xy, 1, function(x)c(rep(x[1]-r[1], 3), rep(x[1]+r[1],3), x[1], x[1], rep(c(x[2]+r[2], x[2], x[2]-r[2]), 2),  x[2]+r[2], x[2]-r[2])))
-	} 
+		fun <- function(x)c(rep(x[1]-r[1], 3), rep(x[1]+r[1],3), x[1], x[1], rep(c(x[2]+r[2], x[2], x[2]-r[2]), 2),  x[2]+r[2], x[2]-r[2])
+	}	
+	d <- t(apply(xy, 1, fun))
 	d <- matrix(as.vector(d), ncol=2)
+	
 	if (.couldBeLonLat(x)) {
 		# normalize longitude to -180..180
 		d[,1] <- (d[,1] + 180) %% 360 - 180
 	}
-	unique(na.omit(cellFromXY(x, d)))
+	
+	if (pairs) {
+		cells <- rep(cells, directions)
+		cells <- na.omit(cbind(cells, cellFromXY(x, d)))
+		colnames(cells) <- c('from', 'to')
+		cells[order(cells[,1], cells[,2]),]
+	} else {
+		as.vector(unique(na.omit(cellFromXY(x, d))))
+	}
 }
 
