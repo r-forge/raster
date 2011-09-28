8a36a894e4fc79a2ef7f367ccfb8f0c3290f1285
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-09-28 03:00:49 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1880 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/adjacent.R
+++ pkg/raster/R/adjacent.R
@@ -4,6 +4,34 @@
 # Licence GPL v3
 
 
+
+.adjacentUD <- function(x, cells, ngb) {
+	# ngb should be a matrix with 
+	# one and only one cell with value 0 (the focal cell), 
+	# at least one cell with value 1 (the adjacent cells)
+	# cells with other values are ignored (not considered adjacent)
+	rs <- res(x)
+
+	rn <- raster(ngb)
+	center <- which(values(rn)==0)
+	rc <- rowFromCell(rn, center)
+	cc <- colFromCell(rn, center)
+	
+	xngb <- yngb <- ngb
+	xngb[] <- rep(1:ncol(ngb), each=nrow(ngb)) - cc 
+	yngb[] <- rep(nrow(ngb):1, ncol(ngb)) - (nrow(ngb)-rc+1)
+	ngb[ngb != 1] <- NA
+	xngb <- as.vector( xngb * rs[1] * ngb)
+	yngb <- as.vector( yngb * rs[2] * ngb)
+	
+	xy <- xyFromCell(x, cells)
+	x <- apply(xy[,1,drop=FALSE], 1, function(z) z + xngb )
+	y <- apply(xy[,2,drop=FALSE], 1, function(z) z + yngb )
+
+	c(as.vector(x), as.vector(y))
+}
+
+
 adjacent <- function(x, cells, directions=4, pairs=TRUE, target=NULL) {
 
 	if (is.character(directions)) { directions <- tolower(directions) }
@@ -12,15 +40,22 @@ adjacent <- function(x, cells, directions=4, pairs=TRUE, target=NULL) {
 	r <- res(x)
 	xy <- xyFromCell(x, cells)
 
+	mat <- FALSE
 	if (is.matrix(directions)) {
-		.adjacentUD(x, cells, directions, pairs=pairs, target=traget)
-	}
-	if (directions==4) {
+		stopifnot(length(which(directions==0)) == 1)
+		stopifnot(length(which(directions==1)) > 0)
+		
+		d <- .adjacentUD(x, cells, directions)
+		
+		directions <- length(directions)
+		mat <- TRUE
+		
+	} else if (directions==4) {
 		d <- c(xy[,1]-r[1], xy[,1]+r[1], xy[,1], xy[,1], xy[,2], xy[,2], xy[,2]+r[2], xy[,2]-r[2])
 		
 	} else if (directions==8) {
-		d <- c(rep(xy[,1]-r[1], 3), rep(xy[,1]+r[1],3), xy[,1], xy[,1], 
-			rep(c(xy[,2]+r[2], xy[,2], xy[,2]-r[2]), 2),  xy[,2]+r[2], xy[,2]-r[2])
+		d <- c(rep(xy[,1]-r[1], 3), rep(xy[,1]+r[1],3), xy[,1], xy[,1],
+				rep(c(xy[,2]+r[2], xy[,2], xy[,2]-r[2]), 2), xy[,2]+r[2], xy[,2]-r[2])
 
 	} else if (directions==16) {
 		r2 <- r * 2
@@ -38,19 +73,23 @@ adjacent <- function(x, cells, directions=4, pairs=TRUE, target=NULL) {
 		directions <- 4 # to make pairs
 		
 	} else {
-		stop('directions should be one of: 4, 8, 16, or bishop')
+		stop('directions should be one of: 4, 8, 16, "bishop", or a matrix')
 	}
 	
 	d <- matrix(d, ncol=2)
-	
 	if (.isGlobalLonLat(x)) {
 		# normalize longitude to -180..180
 		d[,1] <- (d[,1] + 180) %% 360 - 180
 	}
 	
 	if (pairs) {
-		cells <- rep(cells, directions)
+		if (mat) {
+			cells <- rep(cells, each=directions)		
+		} else {
+			cells <- rep(cells, directions)
+		}
 		d <- na.omit(cbind(cells, cellFromXY(x, d)))
+		attr(d, 'na.action') <- NULL
 		colnames(d) <- c('from', 'to')
 		if (! is.null(target)) {
 			d <- d[d[,2] %in% target, ]
@@ -61,6 +100,7 @@ adjacent <- function(x, cells, directions=4, pairs=TRUE, target=NULL) {
 		if (! is.null(target)) {
 			d <- intersect(d, target)
 		}
+		d <- sort(d)
 	}
 	d
 }
