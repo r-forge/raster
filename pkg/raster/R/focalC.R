8683566be30da15e706020a60e76186790da9fd5
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-10-05 02:58:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1895 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/focalC.R
+++ pkg/raster/R/focalC.R
@@ -8,13 +8,27 @@
 
 	out <- raster(x)
 	filename <- trim(filename)
-	globll <- .isGlobalLonLat(out)
+
+	if (length(w) == 1) {
+		w=matrix(1, nc=w, nr=w)
+	} else if (length(w) == 2) {
+		w=matrix(1, nc=w[1], nr=w[2])
+	} 
+	if (! is.matrix(w) ) {
+		stop('w should be a single number, two numbers, or a matrix')
+	} 
+	d <- dim(w)
+	if (d[1] %% 2 == 0 | d[2] %% 2 == 0) {
+		stop('w should have uneven dimensions')
+	}
 	
+
+	gll <- as.integer(.isGlobalLonLat(out))
 	if (canProcessInMemory(out)) {
 		if (missing(fun)) {
-			v <- .Call('focal_sum', values(x), w, as.integer(dim(out)), as.integer(globll), NAOK = TRUE, PACKAGE='raster')
+			v <- .Call('focal_sum', values(x), w, as.integer(dim(out)), gll, NAOK=TRUE, PACKAGE='raster')
 		} else {
-			v <- .Call('focal_fun', values(x), w, as.integer(dim(out)), as.integer(globll), fun, new.env(), NAOK = TRUE, PACKAGE='raster')
+			v <- .Call('focal_fun', values(x), w, as.integer(dim(out)), gll, fun, new.env(), NAOK=TRUE, PACKAGE='raster')
 		}
 		out <- setValues(out, v)
 		if (filename  != '') {
@@ -28,6 +42,7 @@
 		}
 		out <- writeStart(out, filename)
 		tr <- blockSize(out, minblocks=3, minrows=3)
+		pb <- pbCreate(tr$n, type=.progress(...))
 
 		addr <- floor(nrow(w) / 2)
 		addc <- floor(ncol(w) / 2)
@@ -43,30 +58,34 @@
 		e <- new.env()
 		v <- getValues(x, row=1, nrows=tr$nrows[1]+addr)
 		if (dofun) {
-			v <- .Call('focal_fun', v, w, as.integer(c(tr$nrows[1]+addr, nc)), as.integer(globll), fun, e, NAOK = TRUE, PACKAGE='raster')
+			v <- .Call('focal_fun', v, w, as.integer(c(tr$nrows[1]+addr, nc)), gll, fun, e, NAOK=TRUE, PACKAGE='raster')
 		} else {
-			v <- .Call('focal_sum', v, w, as.integer(c(tr$nrows[1]+addr, nc)), as.integer(globll), NAOK = TRUE, PACKAGE='raster')
+			v <- .Call('focal_sum', v, w, as.integer(c(tr$nrows[1]+addr, nc)), gll, NAOK=TRUE, PACKAGE='raster')
 		}
 		out <- writeValues(out, v, 1)
+		pbStep(pb)
 		for (i in 2:(tr$n-1)) {
 			v <- getValues(x, row=tr$row[i]-addr, nrows=tr$nrows[i]+(2*addr))
 			if (dofun) {
-				v <- .Call('focal_fun', v, w, as.integer(c(tr$nrows[i]+(2*addr), nc)), as.integer(globll), fun, e, NAOK = TRUE, PACKAGE='raster')
+				v <- .Call('focal_fun', v, w, as.integer(c(tr$nrows[i]+(2*addr), nc)), gll, fun, e, NAOK=TRUE, PACKAGE='raster')
 			} else {
-				v <- .Call('focal_sum', v, w, as.integer(c(tr$nrows[i]+(2*addr), nc)), as.integer(globll), NAOK = TRUE, PACKAGE='raster')
+				v <- .Call('focal_sum', v, w, as.integer(c(tr$nrows[i]+(2*addr), nc)), gll, NAOK=TRUE, PACKAGE='raster')
 			}
 			out <- writeValues(out, v[-nc1], tr$row[i])
+			pbStep(pb) 
 		}
 		i <- tr$n
 		v <- getValues(x, row=tr$row[i]-addr, nrows=tr$nrows[i]+addr)
 		if (dofun) {
-			v <- .Call('focal_fun', v, w, as.integer(c(tr$nrows[i]+addr, nc)), as.integer(globll), fun, e, NAOK = TRUE, PACKAGE='raster')
+			v <- .Call('focal_fun', v, w, as.integer(c(tr$nrows[i]+addr, nc)), gll, fun, e, NAOK=TRUE, PACKAGE='raster')
 		} else {
-			v <- .Call('focal_sum', v, w, as.integer(c(tr$nrows[i]+addr, nc)), as.integer(globll), NAOK = TRUE, PACKAGE='raster')
+			v <- .Call('focal_sum', v, w, as.integer(c(tr$nrows[i]+addr, nc)), gll, NAOK=TRUE, PACKAGE='raster')
 		}
 		out <- writeValues(out, v[-nc1], tr$row[i])
+		pbStep(pb) 
+		
 		out <- writeStop(out)			
-			
+		pbClose(pb)	
 	}
 	return(out)
 }
