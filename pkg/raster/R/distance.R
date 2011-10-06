875f7008a0a490790e63981341fd7c9807f21462
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-10-06 05:55:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1902 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/distance.R
+++ pkg/raster/R/distance.R
@@ -10,9 +10,7 @@ if (!isGeneric("distance")) {
 
 
 setMethod('distance', signature(x='RasterLayer'), 
-
 function(x, filename='', doEdge=FALSE, ...) {
-
 	if (doEdge) {
 		r <- edge(x, classes=FALSE, type='inner', asNA=TRUE, progress=.progress(...)) 
 		pts <- try(  rasterToPoints(r, fun=function(z){ z>0 } )[,1:2, drop=FALSE] )
@@ -23,129 +21,10 @@ function(x, filename='', doEdge=FALSE, ...) {
 	if (class(pts) == "try-error") {
 		return( .distanceRows(x, filename=filename, ...) )
 	}
-
 	if (nrow(pts) == 0) {
 		stop('RasterLayer has no NA cells (for which to compute a distance)')
 	}
-
 	out <- raster(x)
-	
-	if (.couldBeLonLat(out)) { 
-		distfun <- .haversine
-	} else { 
-		distfun <- .planedist
-	}
-	                                                                        
-	filename <- trim(filename)
-	if (!canProcessInMemory(out) && filename == '') {
-		filename <- rasterTmpFile()
-								
-	}
-	
-	if (filename == '') {
-		v <- matrix(ncol=nrow(out), nrow=ncol(out))
- 	} else {
-		out <- writeStart(out, filename, ...)
-	}
-	
-	pb <- pbCreate(nrow(out), type=.progress(...))
-
-	xy <- xFromCol(out, 1:ncol(out))
-	xy <- cbind(xy, NA)
-	
-	if (.doCluster() ) {
-		cl <- getCluster()
-		on.exit( returnCluster() )
-		
-		nodes <- min(nrow(out), length(cl)) # at least 1 row
-		
-		cat('Using cluster with', nodes, 'nodes\n')
-		flush.console()
-		
-		clFun <- function(r) {
-			vals <- getValues(x, r)
-			i <- which(is.na(vals))
-			vals[] <- 0
-			if (length(i) > 0) {
-				xy[,2] <- yFromRow(out, r)
-				for (c in i) {
-					vals[c] <- min( distfun(xy[c,1], xy[c,2], pts[,1], pts[,2]) ) 
-				}
-			}
-			return( vals )
-		}
-	
-        for (i in 1:nodes) {
-			sendCall(cl[[i]], clFun, i, tag=i)
-		}
-
-		if (filename=="") {
-			for (r in 1:nrow(out)) {
-				d <- recvOneData(cl)
-				if (! d$value$success) { stop('cluster error') }
-				v[,d$value$tag] <- d$value$value
-				nr <- nodes + r
-				if (nr <= out@nrows) {
-					sendCall(cl[[d$node]], clFun, nr, tag=nr)
-				}
-				pbStep(pb)
-			}
-			out <- setValues(out, as.vector(v)) 
-			
-		} else {
-			for (r in 1:nrow(out)) {
-				d <- recvOneData(cl)
-				if (! d$value$success) { stop('cluster error') }
-				out <- writeValues(out, as.vector(d$value$value), d$value$tag)
-				nr <- nodes + r
-				if (nr <= out@nrows) {
-					sendCall(cl[[d$node]], clFun, nr, tag=nr)
-				}
-				pbStep(pb, r)
-			}
-			out <- writeStop(out)
-		}
-	
-	} else {	
-	
-		if (filename=="") {
-			for (r in 1:nrow(out)) {	
-				vals <- getValues(x, r)
-				i <- which(is.na(vals))
-				vals[] <- 0
-				if (length(i) > 0) {
-					xy[,2] <- yFromRow(out, r)
-					for (c in i) {
-						#vals[c] <- min( pointDistance(xy[c,], pts, longlat=longlat) )
-						vals[c] <- min( distfun(xy[c,1], xy[c,2], pts[,1], pts[,2]) )
-					}
-				}
-				v[,r] <- vals
-				pbStep(pb, r) 	
-			}
-			out <- setValues(out, as.vector(v)) 
-			
-		} else {
-			for (r in 1:nrow(out)) {	
-				vals <- getValues(x, r)
-				i <- which(is.na(vals))
-				vals[] <- 0
-				if (length(i) > 0) {
-					xy[,2] <- yFromRow(out, r)
-					for (c in i) {
-#						vals[c] <- min( pointDistance(xy[c,], pts, longlat=longlat) )
-						vals[c] <- min( distfun(xy[c,1], xy[c,2], pts[,1], pts[,2]) )
-					}
-				}
-				out <- writeValues(out, vals, r)
-				pbStep(pb, r) 	
-			}
-			out <- writeStop(out)
-		}
-	}
-	pbClose(pb)
-	
-	return(out)
+	distanceFromPoints(out, pts, filename=filename, ...)
 }
 )
-
--- pkg/raster/src/dist_util.h
+++ pkg/raster/src/dist_util.h
@@ -1,3 +1,5 @@
+/* (Euclidian) distance on a plane */
+double distPlane(double x1, double y1, double x2, double y2) ;
 
 /* law of cosines distance */
 double distCos(double lon1, double lat1, double lon2, double lat2, double r) ;
