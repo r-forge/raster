77c24fb072e34512011049a190f47120358cc856
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-10-06 06:04:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1904 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/distanceRows.R
+++ pkg/raster/R/distanceRows.R
@@ -50,7 +50,7 @@
 			to <- xyv[is.na(xyv[,3]), 1:2]
 			v[] = 0
 			if ( isTRUE(nrow(to) > 0) ) {
-				v[is.na(xyv[,3])] <- .Call("distanceToNearestPoint", to[p,], from, as.integer(longlat), PACKAGE='raster')
+				v[is.na(xyv[,3])] <- .Call("distanceToNearestPoint", to, from, as.integer(longlat), PACKAGE='raster')
 			}			
 			if (hasWritten) {
 				# after the first round, compare new values with previously written values
