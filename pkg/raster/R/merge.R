1485b81a67784d73340d89cc7965f9a8d4225b85
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-10-28 20:21:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1971 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/merge.R
+++ pkg/raster/R/merge.R
@@ -1,8 +1,11 @@
-# Authors: Robert J. Hijmans 
-# contact: r.hijmans@gmail.com
+# Author: Robert J. Hijmans 
 # Date : October 2008
 # Version 0.9
 # Licence GPL v3
+
+# redesinged for multiple row processing
+# October 2011
+# version 1
 
 if (!isGeneric("merge")) {
 	setGeneric("merge", function(x, y, ...)
@@ -11,34 +14,42 @@ if (!isGeneric("merge")) {
 
 
 setMethod('merge', signature(x='Raster', y='Raster'), 
-function(x, y, ..., tolerance=0.05, filename="", format, overwrite, progress) { 
+function(x, y, ..., tolerance=0.05, filename="", format, datatype, overwrite, progress) { 
 	
 	x <- c(x, y, list(...))
+	x <- x[ sapply(x, function(x) inherits(x, 'Raster')) ]
+	if (length(x) < 2) {
+		stop('merge needs at least 2 Raster* objects')
+	}
+	
 	if (missing(format)) { format <- .filetype(format=format, filename=filename) } 
 	if (missing(overwrite)) { overwrite <- .overwrite()	}
 	if (missing(progress)) { progress <- .progress() }
-	merge(x, tolerance=tolerance, filename=filename, format=format, overwrite=overwrite, progress=progress, test=FALSE)
+
+	if (missing(datatype)) {
+		dtype <- sapply(x, function(y) raster:::.shortDataType(dataType(y)))
+		if (any(dtype == 'FLT')) {
+				datatype <- 'FLT4S'
+		} else {
+			datatype <- 'INT4S'
+		}
+	} 
+	
+	merge(x, tolerance=tolerance, filename=filename, format=format, datatype=datatype, overwrite=overwrite, progress=progress, test=FALSE)
 
 } )
 
 
 
 setMethod('merge', signature(x='list', y='missing'), 
-function(x, y,..., tolerance=0.05, filename="", format, overwrite, progress, test=TRUE){ 
+function(x, y,..., tolerance=0.05, filename="", format, datatype, overwrite, progress, test=TRUE){ 
 
-	s <- sapply(x, function(x) inherits(x, 'Raster'))
-	x <- x[s]
-
-	if (length(x) < 2) {
-		stop('merge needs at least 2 Raster* objects')
-	}
-	
 	nl <- unique(sapply(x, nlayers))
 	if (length(nl) != 1) {
 		if (length(nl) == 2 & min(nl) == 1) {
 			nl <- max(nl)
 		} else {
-			stop( 'different number of layers' )
+			stop( 'different number of layers among objects' )
 		}
 	}
 	compare(x, extent=FALSE, rowcol=FALSE, orig=TRUE, res=TRUE, tolerance=tolerance)
@@ -47,10 +58,16 @@ function(x, y,..., tolerance=0.05, filename="", format, overwrite, progress, tes
 	if (missing(format)) { format <- .filetype(format=format, filename=filename) } 
 	if (missing(overwrite)) { overwrite <- .overwrite()	}
 	if (missing(progress)) { progress <- .progress() }
-
+	if (missing(datatype)) {
+		dtype <- sapply(x, function(y) raster:::.shortDataType(dataType(y)))
+		if (any(dtype == 'FLT')) {
+				datatype <- 'FLT4S'
+		} else {
+			datatype <- 'INT4S'
+		}
+	} 
 
 	bb <- unionExtent(x)
-	
 	if (nl > 1) {
 		out <- brick(x[[1]], values=FALSE)
 	} else {
@@ -58,17 +75,10 @@ function(x, y,..., tolerance=0.05, filename="", format, overwrite, progress, tes
 	}
 	out <- setExtent(out, bb, keepres=TRUE, snap=FALSE)
 
-	datatype <- 'INT4S'
-	for (i in 1:length(x)) {
-		dtype <- .shortDataType(dataType(x[[i]]))
-		if (any(dtype == 'FLT')) {
-			datatype <- 'FLT4S'
-		}	
-	}
-
+	
 	if ( canProcessInMemory(out, 3) ) {
 		if (nl > 1) {
-			v = matrix(NA, nrow=ncell(out), ncol=nl)
+			v <- matrix(NA, nrow=ncell(out), ncol=nl)
 			for (i in 1:length(x)) {
 				cells <- cellsFromExtent( out, extent(x[[i]]) )
 				vv <- v[cells, ]
@@ -81,12 +91,12 @@ function(x, y,..., tolerance=0.05, filename="", format, overwrite, progress, tes
 				v[cells, ] <- vv
 			}
 		} else {
-			v = rep(NA, ncell(out))
+			v <- rep(NA, ncell(out))
 			for (i in 1:length(x)) {
-				cells = cellsFromExtent( out, extent(x[[i]]) )
-				vv = v[cells]
-				vv[is.na(vv)] = getValues(x[[i]])[is.na(vv)]
-				v[cells] = vv
+				cells <- cellsFromExtent( out, extent(x[[i]]) )
+				vv <- v[cells]
+				vv[is.na(vv)] <- getValues(x[[i]])[is.na(vv)]
+				v[cells] <- vv
 			}
 		}
 		rm(vv)
@@ -98,14 +108,15 @@ function(x, y,..., tolerance=0.05, filename="", format, overwrite, progress, tes
 	}
 	
 	
-	rowcol <- matrix(0, ncol=4, nrow=length(x))
+	rowcol <- matrix(0, ncol=5, nrow=length(x))
 	for (i in 1:length(x)) {
-		xy1 <- xyFromCell(x[[i]], 1) # first row/col on old raster[[i]]
-		xy2 <- xyFromCell(x[[i]], ncell(x[[i]]) )     #last row/col on old raster[[i]]
-		rowcol[i,1] <- rowFromY(out, xy1[2])       #start row on new raster
-		rowcol[i,2] <- rowFromY(out, xy2[2])       #end row
-		rowcol[i,3] <- colFromX(out, xy1[1])       #start col
-		rowcol[i,4] <- rowcol[i,3] + ncol(x[[i]]) - 1  #end col
+		xy1 <- xyFromCell(x[[i]], 1) 					# first row/col on old raster[[i]]
+		xy2 <- xyFromCell(x[[i]], ncell(x[[i]]) )     	#last row/col on old raster[[i]]
+		rowcol[i,1] <- rowFromY(out, xy1[2])       		#start row on new raster
+		rowcol[i,2] <- rowFromY(out, xy2[2])    	   	#end row
+		rowcol[i,3] <- colFromX(out, xy1[1])	       	#start col
+		rowcol[i,4] <- rowcol[i,3] + ncol(x[[i]]) - 1  	#end col
+		rowcol[i,5] <- i 								# layer
 	}
 
 	if (filename == "") {
@@ -115,21 +126,20 @@ function(x, y,..., tolerance=0.05, filename="", format, overwrite, progress, tes
 	out <- writeStart(out, filename=filename, format=format, datatype=datatype, overwrite=overwrite)
 
 	tr <- blockSize(out, minblocks=2)
-	tr$row = sort(unique(c(tr$row, rowcol[,1:2])))
-	tr$nrows = c(tr$row[-1], nrow(out)+1) - c(tr$row)
-	tr$n = length(tr$row)
+	tr$row <- sort(unique(c(tr$row, rowcol[,1], rowcol[,2]+1)))
+	tr$row <- subset(tr$row, tr$row <= nrow(out)) 
+	tr$nrows <- c(tr$row[-1], nrow(out)+1) - c(tr$row)
+	tr$n <- length(tr$row)
 	pb <- pbCreate(tr$n, type=progress)
 
 	if (nl == 1) {
 		for (i in 1:tr$n) {
 			vv <- v <- matrix(NA, nrow=tr$nrow[i], ncol=ncol(out))
-			for (j in length(x):1) {  #reverse order so that the first raster covers the second etc.
-				if (tr$row[i] >= rowcol[j,1] &  tr$row[i] <= rowcol[j,2]) {
-					row1 <- tr$row[i] - rowcol[j,1] + 1
-					vv[] <- NA
-					vv[, rowcol[j,3]:rowcol[j,4]] <- matrix(getValues(x[[j]], row1, tr$nrow[i]), nrow=tr$nrow[i], byrow=TRUE)	
-					v[!is.na(vv)] <- vv[!is.na(vv)]	
-				}
+			rc <- subset(rowcol, tr$row[i] >= rowcol[,1] &  tr$row[i] <= rowcol[,2])			
+			for (j in nrow(rc):1) {  #reverse order so that the first raster covers the second etc.
+				vv[] <- NA
+				vv[, rc[j,3]:rc[j,4]] <- matrix(getValues(x[[ rc[j,5] ]], tr$row[i]-rc[j,1]+1, tr$nrow[i]), nrow=tr$nrow[i], byrow=TRUE)	
+				v[!is.na(vv)] <- vv[!is.na(vv)]	
 			}
 			out <- writeValues(out, as.vector(t(v)), tr$row[i])
 			pbStep(pb, i)
@@ -137,14 +147,12 @@ function(x, y,..., tolerance=0.05, filename="", format, overwrite, progress, tes
 	} else {
 		for (i in 1:tr$n) {
 			vv <- v <- matrix(NA, nrow=tr$nrow[i]*ncol(out), ncol=nl)
-			for (j in length(x):1) {  #reverse order so that the first raster covers the second etc.
-				if (tr$row[i] >= rowcol[j,1] &  tr$row[i] <= rowcol[j,2]) {
-					row1 <- tr$row[i] - rowcol[j,1] + 1
-					vv[] <- NA
-					cells <- cellFromRowColCombine(out, 1:tr$nrow[i], rowcol[j,3]:rowcol[j,4])
-					vv[cells, ] <- getValues(x[[j]], row1, tr$nrow[i])
-					v[!is.na(vv)] <- vv[!is.na(vv)]	
-				}
+			rc <- subset(rowcol, tr$row[i] >= rowcol[,1] &  tr$row[i] <= rowcol[,2])			
+			for (j in nrow(rc):1) { 
+				vv[] <- NA
+				cells <- cellFromRowColCombine(out, 1:tr$nrow[i], rc[j,3]:rc[j,4])
+				vv[cells, ] <- getValues(x[[ rc[j,5] ]], tr$row[i]-rc[j,1]+1, tr$nrow[i])
+				v[!is.na(vv)] <- vv[!is.na(vv)]	
 			}
 			out <- writeValues(out, v, tr$row[i])
 			pbStep(pb, i)
@@ -154,5 +162,4 @@ function(x, y,..., tolerance=0.05, filename="", format, overwrite, progress, tes
 	writeStop(out)
 }
 )
-
 
