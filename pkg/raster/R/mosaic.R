acbea31a4b78282885e7500225361577796e40a2
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-10-31 04:58:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@1980 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/mosaic.R
+++ pkg/raster/R/mosaic.R
@@ -2,7 +2,6 @@
 # Date : October 2008
 # Version 0.9
 # Licence GPL v3
-
 # redesinged for multiple row processing
 # October 2011
 # version 1
@@ -101,7 +100,7 @@ function(x, y,..., fun, tolerance=0.05, filename="", format, datatype, overwrite
 		return(out)
 	}
 
-	rowcol <- matrix(NA, ncol=5, nrow=length(x))
+	rowcol <- matrix(NA, ncol=6, nrow=length(x))
 	for (i in 1:length(x)) {
 		xy1 <- xyFromCell(x[[i]], 1) 				# first row/col on old raster[[i]]
 		xy2 <- xyFromCell(x[[i]], ncell(x[[i]]) )   # last row/col on old raster[[i]]
@@ -110,28 +109,30 @@ function(x, y,..., fun, tolerance=0.05, filename="", format, datatype, overwrite
 		rowcol[i,3] <- colFromX(out, xy1[1])	    # start col
 		rowcol[i,4] <- colFromX(out, xy2[1])		# end col
 		rowcol[i,5] <- i							# layer
+		rowcol[i,6] <- nrow(x[[i]])
 	}
 
-	w <- getOption('warn')
-	on.exit( options('warn'= w) )
-	
-	tr <- blockSize(out, minblocks=2)
-	tr$row <- sort(unique(c(tr$row, rowcol[,1], rowcol[,2]+1)))
-	tr$row <- subset(tr$row, tr$row <= nrow(out)) 
-	tr$nrows <- c(tr$row[-1], nrow(out)+1) - c(tr$row)
-	tr$n <- length(tr$row)
-
+	tr <- blockSize(out)
 	pb <- pbCreate(tr$n, type=progress)
 	out <- writeStart(out, filename=filename, format=format, datatype=datatype, overwrite=overwrite)
+
 	if (nl == 1) {
 		for (i in 1:tr$n) {
-			rc <- subset(rowcol, tr$row[i] >= rowcol[,1] &  tr$row[i] <= rowcol[,2])			
+			rc <- subset(rowcol, (tr$row[i]+tr$nrow[i]-1) > rowcol[,1] &  tr$row[i] < rowcol[,2])
 			if (nrow(rc) > 0) {
 				v <- matrix(NA, nrow=tr$nrow[i] * ncol(out), ncol=nrow(rc))
-				startcell <- cellFromRowCol(out, tr$row[i], 1) - 1
 				for (j in 1:nrow(rc)) {
-					cells <- cellFromRowColCombine(out, tr$row[i]:(tr$row[i]+tr$nrows[i]-1), rc[j,3]:rc[j,4]) - startcell
-					v[cells, j] <- getValues(x[[ rc[j,5] ]], tr$row[i]-rc[j,1]+1, tr$nrow[i])
+				
+					r1 <- tr$row[i]-rc[j,1]+1 
+					r2 <- r1 + tr$nrow[i]-1
+					z1 <- abs(min(1,r1)-1)+1
+					r1 <- max(1, r1)
+					r2 <- min(rc[j,6], r2)
+					nr <- r2 - r1 + 1
+					z2 <- z1 + nr - 1
+				
+					cells <- cellFromRowColCombine(out, z1:z2, rc[j,3]:rc[j,4])
+					v[cells, j] <- getValues(x[[ rc[j,5] ]], r1, nr)
 				}
 				if (rowcalc) {
 					v <- fun(v, na.rm=TRUE)
@@ -146,14 +147,23 @@ function(x, y,..., fun, tolerance=0.05, filename="", format, datatype, overwrite
 		}
 	} else {
 		for (i in 1:tr$n) {
-			rc <- subset(rowcol, tr$row[i] >= rowcol[,1] &  tr$row[i] <= rowcol[,2])			
+			rc <- subset(rowcol, (tr$row[i]+tr$nrow[i]-1) > rowcol[,1] &  tr$row[i] < rowcol[,2])
 			if (nrow(rc) > 0) {
 				v <- matrix(NA, nrow=tr$nrow[i]*ncol(out) * nl, ncol=nrow(rc))
-				startcell <- cellFromRowCol(out, tr$row[i], 1) - 1
 				for (j in 1:nrow(rc)) { 
-					cells <- cellFromRowColCombine(out, tr$row[i]:(tr$row[i]+tr$nrows[i]-1), rc[j,3]:rc[j,4]) - startcell
+
+					r1 <- tr$row[i]-rc[j,1]+1 
+					r2 <- r1 + tr$nrow[i]-1
+					z1 <- abs(min(1,r1)-1)+1
+					r1 <- max(1, r1)
+					r2 <- min(rc[j,6], r2)
+					nr <- r2 - r1 + 1
+					z2 <- z1 + nr - 1
+
+					cells <- cellFromRowColCombine(out, z1:z2, rc[j,3]:rc[j,4])
 					cells <- cells + rep(0:(nl-1)* tr$nrow[i]*ncol(out), each=length(cells))
-					v[cells, j] <- as.vector( getValues(x[[ rc[j,5] ]], tr$row[i]-rc[j,1]+1, tr$nrow[i]) )
+					v[cells, j] <- as.vector( getValues(x[[ rc[j,5] ]], r1, nr) )
+					
 				}
 				if (rowcalc) {
 					v <- fun(v, na.rm=TRUE)
