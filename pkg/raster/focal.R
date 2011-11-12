be7a2503c38c675072827452f3959f4eddb3b200
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-11-12 19:00:01 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2024 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/focal.R
+++ pkg/raster/focal.R
@@ -4,32 +4,22 @@
 # Licence GPL v3
 
 
-.checkngb <- function(ngb) {
-	ngb <- as.integer(round(ngb))
-	if (length(ngb) == 1) {
-		ngb <- c(ngb, ngb)
-	} else if (length(ngb) > 2) {
-		stop('ngb should be a single value or two values')
-	}
-	if (min(ngb) < 1) { stop("ngb should be larger than 1") } 
-	return(ngb)
-}
-
 
 .getW <- function(w) {
 	if (length(w) == 1) {
 		w <- round(w)
 		stopifnot(w > 1)
-		w=matrix(1, ncol=w, nrow=w)
+		w <- matrix(1, ncol=w, nrow=w)
 	} else if (length(w) == 2) {
 		w <- round(w)
-		w=matrix(1, ncol=w[1], nrow=w[2])
+		w <- matrix(1, ncol=w[1], nrow=w[2])
 	} 
 	if (! is.matrix(w) ) {
 		stop('w should be a single number, two numbers, or a matrix')
 	} 
-	return(w)
+	w
 }
+
 
 if (!isGeneric("focal")) {
 	setGeneric("focal", function(x, ...)
@@ -39,7 +29,7 @@ if (!isGeneric("focal")) {
 setMethod('focal', signature(x='RasterLayer'), 
 function(x, w=3, fun, filename='', na.rm=FALSE, pad=FALSE, padValue=NA, NAonly=FALSE, ...) {
 
-	stopifnot(hasValues(x))
+	if (!hasValues(x)) return(x)
 	
 	# mistakes because of differences with old focal and old focalFilter
 	dots <- list(...)
@@ -58,7 +48,7 @@ function(x, w=3, fun, filename='', na.rm=FALSE, pad=FALSE, padValue=NA, NAonly=F
 	# to get the weights in the (by row) order for the C routine
 	# but keeping nrow and ncol as-is
 	w[] <- as.vector(t(w))
-
+		
 	out <- raster(x)
 	filename <- trim(filename)
 	
