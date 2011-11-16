d184c5a2434f1ef2cc2b3b94e0b9695c9c9d43e3
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-11-16 18:37:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2031 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/aggregateSP.R
+++ pkg/raster/R/aggregateSP.R
@@ -1,6 +1,6 @@
 
-setMethod('disaggregate', signature(x='SpatialPolygons', fact='ANY'), 
-function(x, fact=NULL, ...) {
+setMethod('aggregate', signature(x='SpatialPolygons'), 
+function(x, v=NULL, ...) {
 	require(rgeos)
 	
 	if (! .hasSlot(x, 'data') ) {
@@ -12,7 +12,7 @@ function(x, fact=NULL, ...) {
 		return(x)
 	
 	} else {
-		if (is.null(fact)) {
+		if (is.null(v)) {
 			if (version_GEOS0() < "3.3.0") {
 				x <- gUnionCascaded(x)
 			} else {
@@ -20,15 +20,38 @@ function(x, fact=NULL, ...) {
 			}	
 			x <- SpatialPolygonsDataFrame(x, data=data.frame(id=1))
 			return(x)
+			
 		} else {
+			dat <- x@data
+			vl <- length(v)
+			v <- unique(v)
+			if (is.numeric(v)) {
+				v <- round(v)
+				v <- v[v>0 & v <= ncol(dat)]
+				if (length(v) < 1) {
+					stop('v has no valid column numbers')
+				}
+			} else if (is.character(v)) {
+				v <- v[v %in% colnames(dat)]
+				if (length(v) < 1) {
+					stop('v has no valid column names')
+				}
+			}
+			if (length(v) < vl) {
+				warning('not all variables were unique or valid')
+			}
+			
+			dat <- dat[,v]
 			crs <- x@proj4string
-			dat <- x@data[,fact]
 			dc <- apply(dat, 1, function(y) paste(as.character(y), collapse='_'))
 			dc <- data.frame(oid=1:length(dc), v=dc)
 			dc[,2] <- as.integer(dc[,2])
 			ud <- data.frame(v=unique(dc[,2]))
 			md <- merge(dc, ud, by='v')
 			md <- md[order(md$oid), ]
+			id <- md[!duplicated(md[,1]),]
+			id <- id[order(id$v), ]
+			dat <- dat[id[,2], ]
 			
 			if (version_GEOS0() < "3.3.0") {
 				x <- lapply(1:nrow(ud), function(y) gUnionCascaded(x[md[md$v==y,2],]))
@@ -40,9 +63,6 @@ function(x, fact=NULL, ...) {
 			x <- SpatialPolygons(lapply(1:length(x), function(y) Polygons(x[[y]], y)))
 			x@proj4string <- crs
 			
-			id <- md[!duplicated(md[,1]),]
-			id <- id[order(id$v), ]
-			dat <- dat[id[,2], ]
 			rownames(dat) <- 1:nrow(dat)
 			
 			SpatialPolygonsDataFrame(x, data=dat)
