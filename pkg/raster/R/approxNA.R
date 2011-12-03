a8cbb92ec26ac7cbd85888a67ba499a2d735bf51
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-12-03 06:50:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2079 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/approxNA.R
+++ pkg/raster/R/approxNA.R
@@ -7,15 +7,28 @@ if (!isGeneric("approxNA")) {
 
 
 setMethod('approxNA', signature(x='RasterStackBrick'), 
-function(x, filename="", method="linear", yleft, yright, rule=1, f=0, ties=mean,...) { 
+function(x, filename="", method="linear", yleft, yright, rule=1, f=0, ties=mean, z=NULL, ...) { 
 
 	filename <- trim(filename)
 	out <- brick(x, values=FALSE)
 	nl <- nlayers(out)
+	if (nl < 2) {
+		warning('cannot interpolate with a single layer')
+		return(x)
+	}
 	
-	xout <- getZ(x)
-	if (is.null(xout)) {
-		xout <- 1:nl
+	if (is.null(z)) {
+		xout <- getZ(x)
+		if (is.null(xout)) {
+			xout <- 1:nl
+		} else if (length(xout)!= nl) {
+			stop('length of values returned by getZ(x) does not match the number of layers of x')
+		}
+	} else {
+		if (length(z)!= nl) {
+			stop('length of z does not match the number of layers of x')
+		}
+		xout <- z		
 	}
 	
 	ifelse((missing(yleft) & missing(yright)), ylr <- 0L, ifelse(missing(yleft), ylr <- 1L, ifelse(missing(yright), ylr <- 2L, ylr <- 3L)))
