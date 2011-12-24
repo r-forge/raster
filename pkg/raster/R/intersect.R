18e5385417326156493b5a9ed73d7c22d0118db9
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-12-24 08:13:56 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2114 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/intersect.R
+++ pkg/raster/R/intersect.R
@@ -3,85 +3,11 @@
 # Version 1.0
 # Licence GPL v3
 
-
 	
 if (!isGeneric("intersect")) {
 	setGeneric("intersect", function(x, y)
 		standardGeneric("intersect"))
 }	
-
-setMethod('intersect', signature(x='SpatialPolygons', y='SpatialPolygons'), 
-function(x, y) {
-	require(rgeos)
-
-	x <- spChFIDs(x, as.character(1:length(row.names(x))))
-	y <- spChFIDs(y, as.character(1:length(row.names(y))))
-		
-	if (! identical(projection(x), projection(y)) ) {
-		warning('non identical CRS')
-		y@proj4string <- x@proj4string
-	}	
-	
-	subs <- gIntersects(x, y, byid=TRUE)
-	if (sum(subs)==0) {
-		warning('polygons do not intersect')
-		return(NULL)
-	}
-		
-	xdata <- .hasSlot(x, 'data')
-	ydata <- .hasSlot(y, 'data')
-	dat <- NULL
-	if (xdata & ydata) {
-		nms <- .goodNames(c(colnames(x@data), colnames(y@data)))
-		colnames(x@data) <- xnames <- nms[1:ncol(x@data)]
-		colnames(y@data) <- ynames <- nms[(ncol(x@data)+1):length(nms)]
-		dat <- cbind(x@data[NULL, ,drop=FALSE], y@data[NULL, ,drop=FALSE])
-	} else if (xdata) {
-		dat <- x@data[NULL, ,drop=FALSE]
-		xnames <- colnames(dat)
-	} else if (ydata) {
-		dat <- y@data[NULL, ,drop=FALSE]
-		ynames <- colnames(dat)
-	}
-
-	subsx <- apply(subs, 2, any)
-	subsy <- apply(subs, 1, any)
-		
-	int  <- gIntersection(x[subsx,], y[subsy,], byid=TRUE)
-	if (inherits(int, "SpatialCollections")) {
-		if (is.null(int@polyobj)) { # ??
-			warning('polygons do not intersect')
-			return(NULL)
-		}
-		int <- int@polyobj
-	}
-	if (!inherits(int, 'SpatialPolygons')) {
-		warning('polygons do not intersect')
-		return(NULL)
-	}
-
-	if (!is.null(dat)) {
-		ids <- do.call(rbind, strsplit(row.names(int), ' '))
-		rows <- 1:length(ids[,1])
-		if (xdata) {
-			idsx <- match(ids[,1], rownames(x@data))
-			dat[rows, xnames] <- x@data[idsx, ]
-		} 
-		if (ydata) {
-			idsy <- match(ids[,2], rownames(y@data))
-			dat[rows, ynames] <- y@data[idsy, ]
-		}
-
-		rownames(dat) <- 1:nrow(dat)
-		int <- SpatialPolygonsDataFrame(int, dat)
-		
-	}
-		#aggregate(res, v=colnames(res@data))
-	int	
-} 
-)
-
-
 
 
 setMethod('intersect', signature(x='Extent', y='Extent'), 
@@ -100,3 +26,5 @@ function(x, y) {
 	return(e)
 	
 } )
+
+
---
+++ pkg/raster/R/union.R
@@ -0,0 +1,15 @@
+# Author: Robert J. Hijmans
+# Date : December 2011
+# Version 1.0
+# Licence GPL v3
+
+if (!isGeneric("union")) {
+	setGeneric("union", function(x, y)
+		standardGeneric("union"))
+}	
+
+setMethod('union', signature(x='Extent', y='Extent'), 
+function(x, y, ...) { 
+	unionExtent(x, y, ...)
+} )
+
