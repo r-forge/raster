790c21a47963ce32ad47d5299f91668752a424ac
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2012-04-25 17:23:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2228 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/layerStats.R
+++ pkg/raster/R/layerStats.R
@@ -1,24 +1,10 @@
-#' Weighted Covariance Matrices and Means for Rasters
-#' 
-#' Calculates the weighted covariance and (optionally) weighted means of bands in an Raster.
-#' 
-#' @param dm A multiband Raster* to use in calculating weighted covariance matrices.
-#' @param wt A Raster* object of the weights (should have the same extent as dm).
-#' @param return_means Logical. Return the weighted per-band means?
-#' @return Returns either a covariance matrix of dimensions nlayers(dm) x nlayers(dm) or a list containing the covariance matrix and the weighted per-band means.
-#' @author Mort Canty (original code) and Jonathan A. Greenberg (R port).
-#' @seealso \code{\link{cov.wt}}, \code{\link{weighted.mean}}
-#' @references
-#' \itemize{
-#' \item {Canty, M.J. and A.A. Nielsen. 2008. Automatic radiometric normalization of multitemporal satellite imagery with the iteratively re-weighted MAD transformation. Remote Sensing of Environment 112:1025-1036.}
-#' \item {Nielsen, A.A. 2007. The regularized iteratively reweighted MAD method for change detection in multi- and hyperspectral data. IEEE Transactions on Image Processing 16(2):463-478.}
-#' }
-# @keywords {weighted covariance matrix}
-# {weighted means}
-# @examples
-# \dontrun{
-# } 
-#' @export
+# Jonathan Greenberg and Robert Hijmans
+# Date : April 2012
+# Version 1.0
+# Licence GPL v3
+
+# Computation of the weighted covariance and (optionally) weighted means of bands in an Raster.
+# based on code by Mort Canty
 
 
 layerStats <- function(x, stat, w, asSample=TRUE, na.rm=TRUE, ...) {
@@ -45,8 +31,10 @@ layerStats <- function(x, stat, w, asSample=TRUE, na.rm=TRUE, ...) {
 			w <- mask(w, nas)
 		}
 
-		sumw <- cellStats(w, stat='sum', na.rm=na.rm) - asSample
+		sumw <- cellStats(w, stat='sum', na.rm=na.rm) 
 		means <- cellStats(x * w, stat='sum', na.rm=na.rm) / sumw
+		sumw <- sumw - asSample
+		
 		x <- (x - means) * sqrt(w)
 		
 		for(i in 1:nl) {
