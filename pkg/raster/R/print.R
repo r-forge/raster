52e1a3ff316d15ee99a67c6a4af58e743c715b74
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2012-05-04 23:56:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2252 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/print.R
+++ pkg/raster/R/print.R
@@ -42,23 +42,23 @@ setMethod ('print' , 'Spatial',
 	
 		cat('class       :' , class(x), '\n')
 		isRaster <- hasData <- FALSE
+		nc <- 0
 		if (.hasSlot(x, 'data')) {
 			hasData <- TRUE
+			nc <- ncol(x@data)
 		}
 		
 		if (inherits(x, 'SpatialPixels')) {
 			isRaster <- TRUE
 			cr <- x@grid@cells.dim
-			nl <- ifelse(hasData, ncol(x@data), 0)
-			cat ('dimensions  : ', cr[2], ', ', cr[1], ', ', nrow(x@coords), ', ', nl, '  (nrow, ncol, npixels, nlayers)\n', sep="" ) 
+			cat ('dimensions  : ', cr[2], ', ', cr[1], ', ', nrow(x@coords), ', ', nc, '  (nrow, ncol, npixels, nlayers)\n', sep="" ) 
 			cs <- x@grid@cellsize
 			cat ('resolution  : ', cs[1], ', ', cs[2], '  (x, y)\n', sep="")		
 
 		} else if (inherits(x, 'SpatialGrid')) {
 			isRaster <- TRUE
 			cr <- x@grid@cells.dim
-			nl <- ifelse(hasData, ncol(x@data), 0)
-			cat ('dimensions  : ', cr[2], ', ', cr[1], ', ', prod(cr), ', ', nl, '  (nrow, ncol, ncell, nlayers)\n', sep="" ) 
+			cat ('dimensions  : ', cr[2], ', ', cr[1], ', ', prod(cr), ', ', nc, '  (nrow, ncol, ncell, nlayers)\n', sep="" ) 
 			cs <- x@grid@cellsize
 			cat ('resolution  : ', cs[1], ', ', cs[2], '  (x, y)\n', sep="")		
 			
@@ -69,36 +69,41 @@ setMethod ('print' , 'Spatial',
 		e <- bbox(x)
 		cat('extent      : ' , e[1], ', ', e[2], ', ', e[3], ', ', e[4], '  (xmin, xmax, ymin, ymax)\n', sep="")
 		cat('coord. ref. :' , projection(x, TRUE), '\n')
-		if (.hasSlot(x, 'data')) {
-			if (!isRaster) {
-				cat('data dims   : ', nrow(x), ', ', ncol(x), '  (nrow, ncol)\n', sep="" ) 
-			}
+		
+		
+		if (hasData) {
+			x <- x@data
 			
-			nfact <- sapply(1:ncol(x@data), function(i) is.numeric(x@data[,i]))
-			lf <- length(nfact)
-			if (lf > 15) {
-				nfact <- nfact[1:15]
+			maxnl <- 15
+			
+			if (! isRaster) {
+				cat('nvariables  : ', nc, '\n', sep="" ) 
 			}
+			if (nc > maxnl) {
+				x <- x[, 1:maxnl]
+			}
+			coln <- colnames(x)
+			if (nc > maxnl) {
+				coln <- c(coln[1:maxnl], '...')
+				
+			}
+			cat('variables   :', paste(coln, collapse=', '), '\n')
+			
+			nfact <- sapply(1:ncol(x), function(i) is.numeric(x[,i]))
 			if (sum(nfact) > 1) {
-				r <- apply(x@data[,which(nfact)], 2, range, na.rm=TRUE)
+				r <- apply(x[,which(nfact)], 2, range, na.rm=TRUE)
 				fc <- as.character(nfact)
 				fc[! nfact] <- '(f)'
 				maxv <- minv <- fc
 				minv[nfact] <- as.vector(r[1, ])
 				maxv[nfact] <- as.vector(r[2,])
-				if (lf > 15) {
+				if (nc > maxnl) {
 					minv <- c(minv, '...')
 					maxv <- c(maxv, '...')
 				}
 				cat('min values  :', paste(minv, collapse=', '), '\n')
 				cat('max values  :', paste(maxv, collapse=', '), '\n')
 			} 
-				
-			coln <- colnames(x@data)
-			if (length(coln) > 10) {
-				coln <- c(coln[1:10], '...')
-			}
-			cat('variables   :', paste(coln, collapse=', '), '\n')
 		}
 	}
 )	
