70da631181a352035edaecf004fdc186c01347c3
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2012-05-18 19:29:21 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2279 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/shp.R
+++ pkg/raster/R/shp.R
@@ -4,12 +4,25 @@
 # Licence GPL v3
 
 
-shapefile <- function(filename, verbose=FALSE) {
+shapefile <- function(filename, object, overwrite=FALSE, verbose=FALSE) {
 	if (!(require(rgdal))) {
 		stop('This function requires the rgdal package; please install it')
 	}
-	fn <- basename(filename) 
-	extension(fn) <- ''
-	readOGR(dirname(filename), fn, stringsAsFactors=FALSE, verbose=verbose) 
+	if (missing(object)) {
+		fn <- basename(filename) 
+		extension(fn) <- ''
+		readOGR(dirname(filename), fn, stringsAsFactors=FALSE, verbose=verbose) 
+	} else {
+		extension(filename) <- '.shp'
+		if (file.exists(filename)) {
+			if (!overwrite) {
+				stop('file exists, use overwrite=TRUE to overwrite it')
+			} 			
+		}
+		layer <- basename(filename)
+		extension(layer) <- ''
+		writeOGR(object, filename, layer, driver='ESRI Shapefile', verbose=verbose, overwrite_layer=overwrite)
+	}
 }
+
 
