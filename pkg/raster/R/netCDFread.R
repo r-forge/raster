e1312fe57d55a763e84869eecdd1736bcb988c9c
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2012-11-06 19:01:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2586 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/netCDFread.R
+++ pkg/raster/R/netCDFread.R
@@ -182,20 +182,21 @@
 				d[d == navalue] <- NA
 				return(d)
 			}
-		} else {
+		} else { # length(dims) == 1
 			v <- matrix(nrow=nrows*ncols, ncol=n)
 			if ( x@file@toptobottom & nrows > 1) {
 				d <- rev(d)
 			}
-			v[] <- d[, alyrs,drop=FALSE]
+			
+			v[] <- d # d[, alyrs, drop=FALSE]
 		}
 	} else {
 		if ( x@file@toptobottom ) { 
-			v <- as.vector( d[, ncol(d):1] )
-		} else {
-			v <- as.vector(d)
-		}
-		v <- matrix(v, ncol=1)
+			if (is.matrix(d)) {
+				d <- d[, ncol(d):1]
+			}
+		} 
+		v <- matrix(as.vector(d), ncol=1)
 		#v <- v[,lyrs,drop=FALSE]
 	}
 	
