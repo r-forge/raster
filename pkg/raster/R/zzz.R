b4730604897900a76ea67741181915c6f5eb9310
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2012-12-29 01:33:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2621 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/zzz.R
+++ pkg/raster/R/zzz.R
@@ -5,7 +5,10 @@
 	packageStartupMessage(paste("raster ", pkg.info[["Version"]], " (", pkg.info["Date"], ")", sep=""))
 
 	tst <- try( removeTmpFiles( .tmptime() ), silent=TRUE ) 
-
+	wd <- getwd()
+	options('startup.working.directory'=wd)
+	fn <- paste(wd, '/rasterOptions_', pkg.info[["Version"]], sep='')
+	.loadOptions(fn)
 	return(invisible(0))
 }
 
