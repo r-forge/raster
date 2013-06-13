6340579740b206323301c23606a07691c39c9296
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2013-06-13 03:26:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2760 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/hdr.R
+++ pkg/raster/R/hdr.R
@@ -4,21 +4,23 @@
 # Licence GPL v3
 
  
-hdr <- function(x, format, extension='.wld', filename) {
+hdr <- function(x, format, extension='.wld') {
 
 	if (inherits(x, 'RasterStack')) { stop('Only applicable to RasterLayer and RasterBrick classes (and their derivatives)') }
-	
-	if (missing(filename)) {
-		if (x@file@name == '') { 
-			stop('Object has no filename; please provide a filemae= argument') 
-		}
-	} else {
-		fn <- trim(as.character(filename[1]))
-		if (nchar(fn) < 1) {
-			stop('invalid filename')
-		}
-		x@file@name == fn
-	}
+
+	if (x@file@name == '') { stop('Object has no filename') }
+
+#	if (missing(filename)) {
+#		if (x@file@name == '') { 
+#			stop('Object has no filename; please provide a "filename=" argument') 
+#		}
+#	} else {
+#		fn <- trim(as.character(filename[1]))
+#		if (nchar(fn) < 1) {
+#			stop('invalid filename')
+#		}
+#		x@file@name == fn
+#	}
 	
 	type <- toupper(format)
 	if (type=="RASTER") {
