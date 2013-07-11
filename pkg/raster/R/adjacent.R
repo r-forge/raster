8436eaae39be856cba0738cce72213052a60705a
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2013-07-11 17:51:52 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2786 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/adjacent.R
+++ pkg/raster/R/adjacent.R
@@ -24,14 +24,14 @@
 	xngb[] <- rep(1:ncol(ngb), each=nrow(ngb)) - cc 
 	yngb[] <- rep(nrow(ngb):1, ncol(ngb)) - (nrow(ngb)-rc+1)
 	ngb[ngb != 1] <- NA
-	xngb <- as.vector( xngb * rs[1] * ngb)
-	yngb <- as.vector( yngb * rs[2] * ngb)
-	
+	xngb <- na.omit(as.vector( xngb * rs[1] * ngb))
+	yngb <- na.omit(as.vector( yngb * rs[2] * ngb))
+		
 	xy <- xyFromCell(x, cells)
-	x <- apply(xy[,1,drop=FALSE], 1, function(z) z + xngb )
-	y <- apply(xy[,2,drop=FALSE], 1, function(z) z + yngb )
+	X <- apply(xy[,1,drop=FALSE], 1, function(z) z + xngb )
+	Y <- apply(xy[,2,drop=FALSE], 1, function(z) z + yngb )
 
-	na.omit(c(as.vector(x), as.vector(y)))
+	c(as.vector(X), as.vector(Y))
 }
 
 
