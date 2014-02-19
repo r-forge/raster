ea4bf5e6691152856217a3855f066465261e4b76
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2014-02-19 06:20:45 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2950 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/raster.R
+++ pkg/raster/R/raster.R
@@ -12,7 +12,7 @@ if ( !isGeneric("raster") ) {
 
 
 setMethod('raster', signature(x='missing'), 
-	function(nrows=180, ncols=360, xmn=-180, xmx=180, ymn=-90, ymx=90, crs, ext, resolution, values=NULL) {
+	function(nrows=180, ncols=360, xmn=-180, xmx=180, ymn=-90, ymx=90, crs, ext, resolution, vals=NULL) {
 		if (missing(ext)) {
 			ext <- extent(xmn, xmx, ymn, ymx)
 		}
