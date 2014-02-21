69f592f6ea23bf8dbd3751c3d1ad9d650673f875
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2014-02-21 23:33:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@2954 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/corr.R
+++ pkg/raster/R/corr.R
@@ -6,33 +6,22 @@ if ( !isGeneric("corr") ) {
 
 
 setMethod('corr', signature(x='RasterLayer', y='RasterLayer'), 
-	function(x, y,...) {
-	
-		compareRaster(x, y, crs=FALSE, stopiffalse=TRUE)
-		
-		if (canProcessInMemory(x, 8)) {
-			s <- na.omit(getValues(stack(x, y)))
-			cor(s[,1], s[,2])
-		} else {
-			s <- stack(x,y)
-			msk <- sum(s, na.rm=FALSE)
-			s <- mask(s, msk)
-			x <- s[[1]]
-			y <- s[[2]]
-			mx <- cellStats(x, mean)
-			my <- cellStats(y, mean)
-			sx <- cellStats(x, sd)
-			sy <- cellStats(y, sd)
-			cellStats(((x - mx) * (y - my)) / (sx * sy), sum)/ (ncell(x)-1)
-		}
+	function(x, y, n=Inf, ...) {
+		corr(stack(x, y), n=n, ...)
 	}
 )
 
 
 setMethod('corr', signature(x='RasterStackBrick', y='missing'), 
-	function(x, y, ...) {
-		n <- nlayers(x)
-		if (n < 2) return(1)
+	function(x, y, n=Inf, ...) {
+		
+		nl <- nlayers(x)
+		if (nl < 2) return(1)
+		
+		if (n < ncell(x)) {
+			x <- sampleRegular(x, size=n, asRaster=TRUE)
+		}
+		
 		if (canProcessInMemory(x, nlayers(x)*4)) {
 			s <- na.omit(getValues(x))
 			s <- cor(s)
@@ -43,15 +32,19 @@ setMethod('corr', signature(x='RasterStackBrick', y='missing'),
 			sx <- cellStats(x, sd)
 			nc <- ncell(x)
 			s <- matrix(NA, nrow=n, ncol=n)
-			for (i in 1:(n-1)) {
-				for (j in (i+1):n) {
+			for (i in 1:(nl-1)) {
+				for (j in (i+1):nl) {
 					s[j,i] <- s[i,j] <- cellStats(((x[[i]] - mx[i]) * (x[[j]] - mx[j])) / (sx[i] * sx[j]), sum)/ (nc-1)
 				}
 			}
 			diag(s) <- 1			
 		}
-		colnames(s) <- rownames(s) <- names(x)
-		s		
+		if (nrow(s) == 2) {
+			s[2,1]
+		} else {
+			colnames(s) <- rownames(s) <- names(x)
+			s		
+		}
 	}
 )
 
