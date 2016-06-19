d7206db061ccb05be79f25ab142dc019b700f283
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2016-06-19 20:48:16 +0000

    kerndens
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@3304 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/raster/R/kernelDensity.R
@@ -0,0 +1,15 @@
+# Author: Robert J. Hijmans
+# Date :  June 2016
+# Version 0.1
+# Licence GPL v3
+
+
+kernelDensity <- function(xy, r, bandwidth) {
+    lims <- as.vector(extent(r)) + rep(res(r), each=2) * c(0.5,-0.5)
+    n <- rev(dim(r)[1:2])
+    xy <- .pointsToMatrix(xy)
+		k <- raster( MASS::kde2d(xy[,1], xy[,2], h=bandwith, n=n, lims=lims) )
+		# to avoid possible small changes due to floating point math and to transfer CRS
+		setValues(r, getValues(k)) 
+}
+
