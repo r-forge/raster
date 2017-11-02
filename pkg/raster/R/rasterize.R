4f2e7e3e08a3e3341aa3677c83e6892e293c9277
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2017-11-02 02:20:44 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@3330 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/rasterize.R
+++ pkg/raster/R/rasterize.R
@@ -40,7 +40,7 @@ setMethod('rasterize', signature(x='SpatialPoints', y='Raster'),
 
 setMethod('rasterize', signature(x='SpatialLines', y='Raster'), 
 	function(x, y, field, fun='last', background=NA, mask=FALSE, update=FALSE, updateValue='all', filename="", ...){ 
-	
+
 		.linesToRaster(x, y, field=field, fun=fun, background=background, mask=mask, update=update, updateValue=updateValue, filename=filename, ...)
 	
 	}
@@ -57,7 +57,7 @@ setMethod('rasterize', signature(x='SpatialPolygons', y='Raster'),
 
 setMethod('rasterize', signature(x='Extent', y='Raster'), 
 	function(x, y, field, fun='last', background=NA, mask=FALSE, update=FALSE, updateValue='all', filename="", getCover=FALSE, silent=TRUE, ...){ 
-	
+		# this could be done much more efficiently.
 		x <- as(x, 'SpatialPolygons')
 		.polygonsToRaster(x, y, field=field, fun=fun, background=background, mask=mask, update=update, updateValue=updateValue, filename=filename, getCover=getCover, silent=silent,...)
 	
