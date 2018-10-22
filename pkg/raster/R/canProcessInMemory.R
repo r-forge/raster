9d3c3a2da70bed3ad45260162880a208bc4dd4cf
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2018-10-22 04:10:35 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/raster@3390 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/canProcessInMemory.R
+++ pkg/raster/R/canProcessInMemory.R
@@ -16,55 +16,50 @@ canProcessInMemory <- function(x, n=4) {
 #	rasterOptions(overwrite=TRUE)
 #  rasterOptions(todisk=TRUE)
 #  return(FALSE)
-	
 	if (.toDisk()) { 
 		return(FALSE) 
 	} 
 	
-	n <- n + nlayers(x) - 1
-	cells <- round( 1.1 * ncell(x) ) * n
+	n <- n + nlayers(x)
+	memneed <- ncell(x) * n * 8
+	
+	if (.estimateMem()) {
+	
+		if ( .Platform$OS.type == "windows" ) {
+		
+			mem <- system2("wmic", args = "OS get FreePhysicalMemory /Value", stdout = TRUE)
+			mem3 <- gsub("\r", "", mem[3])
+			mem3 <- gsub("FreePhysicalMemory=", "", mem3)
+			memavail <- as.numeric(mem3)
 
-	if ( cells > .maxmemory() ) {
-		return(FALSE) 
+			#memavail <- 0.5 * (utils::memory.size(NA) - utils::memory.size(FALSE))
+		} else { #if ( .Platform$OS.type == "unix" ) {
+			memavail <- as.numeric(system("awk '/MemFree/ {print $2}' /proc/meminfo", intern=TRUE))
+		} #else {
+			# mac? or is that also "unix"
+		#}
+
+		# can't use all of it
+		memavail <- 0.75 * memavail
+		
+		#print(paste("mem available:", memavail))
+		#print(paste("mem needed:", memneed))
+
+	
+		if (memneed > memavail) {
+			# options(rasterChunkSize = memavail * 0.5 )
+			return(FALSE)
+		} else {
+			return(TRUE)
+		}
+	
 	} else {
-		return(TRUE)
+		
+		if ( memneed > .maxmemory() ) {
+			return(FALSE) 
+		} else {
+			return(TRUE)
+		}
 	}
 }
 
-#	if (cells > .maxmemory()) {
-#		return(FALSE) 
-#	} else if ( cells < 1000000 ) {
-#		return(TRUE)
-#	} else {
-#		return(TRUE)
-#	}
-	
-	
-#	if (substr( R.Version()$platform, 1, 7) == "i386-pc" ) {
-#	# windows, function memory.size  available
-#	memneed <- cells * 8 * n / (1024 * 1024)
-#	memavail <- 0.5 * (memory.size(NA)-memory.size(FALSE))
-#	if (memneed > memavail) {
-#		return(FALSE)
-#	} else {
-#		return(TRUE)
-#	}
-#   } else {
-
-#	g <- gc()
-
-
-#  if (.Platform$OS.type == "unix"){
-## Memory in KB, from: http://stackoverflow.com/questions/2441046/how-to-get-physical-memory-in-bash
-#	mem <- as.numeric(system("grep MemTotal /proc/meminfo | awk '{print $2}'",intern=TRUE))
-
-#	w <- getOption('warn')
-#	on.exit(options('warn'= w))
-#	options('warn'=-1) 
-#	r <- try( matrix(0.1, ncol=n, nrow=cells), silent=TRUE )
-
-#	if (class(r) == "try-error") {
-#		return( FALSE )
-#		g <- gc()
-#		return( TRUE ) 
-#	}
